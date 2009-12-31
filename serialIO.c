1427914edc3ce3dc996a48cefdbe534f5d475a5f
Ben Levitt <benjie@gmail.com>
2009-12-31 01:31:15 +0000

    Add a missing comma to the Ardustation serial output. Thanks to Tom for finding and reporting this bug.
    git-svn-id: https://gentlenav.googlecode.com/svn/trunk/MatrixPilot@226 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

--- serialIO.c
+++ serialIO.c
@@ -189,7 +189,7 @@ void serial_output_gps( void )
 	}
 	else
 	{
-		serial_output("!!!LAT:%li,LON:%li,SPD:%.2f,CRT:%.2f,ALT:%li,ALH:%i,CRS:%u,BER:%i,WPN:%i,DST:%i***\r\n"
+		serial_output("!!!LAT:%li,LON:%li,SPD:%.2f,CRT:%.2f,ALT:%li,ALH:%i,CRS:%u,BER:%i,WPN:%i,DST:%i,***\r\n"
 					  "+++THH:%i,RLL:%li,PCH:%li,STT:%i,***\r\n",
 			INT_DIVIDE_R(lat_gps.WW, 10) , INT_DIVIDE_R(long_gps.WW, 10) , (sog_gps.BB / 100.0), (climb_gps.BB / 100.0),
 			INT_DIVIDE_R(alt_sl_gps.WW, 100), desiredHeight, INT_DIVIDE_R(cog_gps.BB, 100) , bearing_to_origin,
