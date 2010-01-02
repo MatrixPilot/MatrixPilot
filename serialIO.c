a614f535a05cb64bdfbc59e07cb52c588062aeb1
Pete Hollands <peter.hollands@gmail.com>
2010-01-02 20:44:24 +0000

    Take out INT_DIVIDE Macros to remove bug. Divide CRS by 100 in SERIAL_ARDUSTATION.
    git-svn-id: https://gentlenav.googlecode.com/svn/trunk/MatrixPilot@228 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

--- serialIO.c
+++ serialIO.c
@@ -182,19 +182,19 @@ void serial_output_gps( void )
 	if (++skip < GPS_RATE)
 	{
 		serial_output("+++THH:%i,RLL:%li,PCH:%li,STT:%i,***\r\n",
-			INT_DIVIDE_R(pwOut[THROTTLE_OUTPUT_CHANNEL] - pwTrim[THROTTLE_OUTPUT_CHANNEL], 20),
+			(int)((pwOut[THROTTLE_OUTPUT_CHANNEL] - pwTrim[THROTTLE_OUTPUT_CHANNEL])/20),
 			earth_roll, earth_pitch,
 			mode
 		) ;
 	}
 	else
 	{
-		serial_output("!!!LAT:%li,LON:%li,SPD:%.2f,CRT:%.2f,ALT:%li,ALH:%i,CRS:%u,BER:%i,WPN:%i,DST:%i,***\r\n"
+		serial_output("!!!LAT:%li,LON:%li,SPD:%.2f,CRT:%.2f,ALT:%li,ALH:%i,CRS:%.2f,BER:%i,WPN:%i,DST:%i,***\r\n"
 					  "+++THH:%i,RLL:%li,PCH:%li,STT:%i,***\r\n",
-			INT_DIVIDE_R(lat_gps.WW, 10) , INT_DIVIDE_R(long_gps.WW, 10) , (sog_gps.BB / 100.0), (climb_gps.BB / 100.0),
-			INT_DIVIDE_R(alt_sl_gps.WW - alt_origin.WW, 100), desiredHeight, INT_DIVIDE_R(cog_gps.BB, 100) , bearing_to_origin,
+			lat_gps.WW / 10 , long_gps.WW / 10 , (sog_gps.BB / 100.0), (climb_gps.BB / 100.0),
+			(alt_sl_gps.WW - alt_origin.WW) / 100, desiredHeight, (float)(cog_gps.BB / 100.0), bearing_to_origin,
 			waypointIndex, tofinish,
-			INT_DIVIDE_R(pwOut[THROTTLE_OUTPUT_CHANNEL] - pwTrim[THROTTLE_OUTPUT_CHANNEL], 20),
+			(int)((pwOut[THROTTLE_OUTPUT_CHANNEL] - pwTrim[THROTTLE_OUTPUT_CHANNEL])/20),
 			earth_roll, earth_pitch,
 			mode
 		) ;
