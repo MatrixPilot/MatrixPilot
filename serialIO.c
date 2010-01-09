ea1a8f774bce941689bb7e7463e94926ad74b65c
Ben Levitt <benjie@gmail.com>
2010-01-09 19:27:21 +0000

    Backport ArduStation format bug fixes to 2.0.1.
    git-svn-id: https://gentlenav.googlecode.com/svn/tags/MatrixPilot_2_0_1@243 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

--- serialIO.c
+++ serialIO.c
@@ -194,10 +194,10 @@ void serial_output_gps( void )
 	}
 	else
 	{
-		serial_output("!!!LAT:%li,LON:%li,SPD:%.2f,CRT:%.2f,ALT:%li,ALH:%i,CRS:%.2f,BER:%i,WPN:%i,DST:%i***\r\n"
+		serial_output("!!!LAT:%li,LON:%li,SPD:%.2f,CRT:%.2f,ALT:%li,ALH:%i,CRS:%.2f,BER:%i,WPN:%i,DST:%i,***\r\n"
 					  "+++THH:%i,RLL:%li,PCH:%li,STT:%i,***\r\n",
 			lat_gps.WW / 10 , long_gps.WW / 10 , (sog_gps.BB / 100.0), (climb_gps.BB / 100.0),
-			alt_sl_gps.WW / 100, desiredHeight, (float)(cog_gps.BB), bearing_to_origin,
+			(alt_sl_gps.WW - alt_origin.WW) / 100, desiredHeight, (float)(cog_gps.BB), bearing_to_origin,
 			waypointIndex, tofinish,
 			(int)((pwOut[THROTTLE_OUTPUT_CHANNEL] - pwTrim[THROTTLE_OUTPUT_CHANNEL])/20),
 			earth_roll, earth_pitch,
