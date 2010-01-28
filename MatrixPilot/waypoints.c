34bd103b1d16d2d787c36baaf9abf676b0c4eb72
Ben Levitt <benjie@gmail.com>
2010-01-28 23:20:59 +0000

    Linear interpolation of altitude between waypoints
    git-svn-id: https://gentlenav.googlecode.com/svn/trunk@268 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

--- MatrixPilot/waypoints.c
+++ MatrixPilot/waypoints.c
@@ -13,6 +13,7 @@ int waypointIndex = 0 ;
 struct waypointparameters goal ;
 struct relative2D togoal = { 0 , 0 } ;
 int tofinish  = 0 ;
+int progress_to_goal = 0 ;
 int crosstrack = 0 ;
 signed char desired_dir_waypoint = 0 ;
 
@@ -20,6 +21,8 @@ extern signed char bearing_to_origin ;
 
 
 #if ( WAYPOINT_TYPE == WP_ABSOLUTE )
+// If using absolute waypoints, ABS_TO_REL_IF_NEEDED() is a function that converts 
+// a waypoint from absolute to relative.
 struct waypoint3D ABS_TO_REL_IF_NEEDED(struct waypoint3D wp)
 {
 	union longww accum_nav;
@@ -32,7 +35,10 @@ struct waypoint3D ABS_TO_REL_IF_NEEDED(struct waypoint3D wp)
 	
 	return wp;
 }
+
 #elif ( WAYPOINT_TYPE == WP_RELATIVE )
+// If using relative waypoints, ABS_TO_REL_IF_NEEDED() is a macro that just
+// passes the relative waypoint through unchanged.
 #define ABS_TO_REL_IF_NEEDED(x) (x)
 #endif
 
@@ -43,9 +49,11 @@ void set_goal( struct waypoint3D fromPoint , struct waypoint3D toPoint )
 	goal.x = toPoint.x ;
 	goal.y = toPoint.y ;
 	goal.height = toPoint.z ;
+	goal.fromHeight = fromPoint.z ;
 	courseLeg.x = toPoint.x - fromPoint.x ;
 	courseLeg.y = toPoint.y - fromPoint.y ;
 	goal.phi = rect_to_polar ( &courseLeg ) ;
+	goal.legDist = courseLeg.x ;
 	goal.cosphi = cosine( goal.phi ) ;
 	goal.sinphi = sine( goal.phi ) ;
 	return ;
@@ -54,7 +62,7 @@ void set_goal( struct waypoint3D fromPoint , struct waypoint3D toPoint )
 void init_waypoints ( void )
 {
 	waypointIndex = 0 ;
-	set_goal ( GPSlocation , ABS_TO_REL_IF_NEEDED(waypoints[0].loc) ) ;
+	set_goal( GPSlocation , ABS_TO_REL_IF_NEEDED(waypoints[0].loc) ) ;
 	desired_behavior.W = waypoints[0].flags ;
 	return ;
 }
@@ -62,50 +70,47 @@ void init_waypoints ( void )
 void compute_waypoint ( void )
 {
 	union longww temporary ;
-	{
-
-		// compute the goal vector from present position to waypoint target in meters:
-
-		togoal.x =  goal.x  - GPSlocation.x  ;
-		togoal.y =  goal.y  - GPSlocation.y  ;
-
-		// project the goal vector onto the direction vector between waypoints
-		// to get the distance to the "finish" line:
-
-		temporary.WW = (	__builtin_mulss( togoal.x , goal.cosphi )
-					+ __builtin_mulss( togoal.y , goal.sinphi ))<<2 ;
-
-		tofinish = temporary._.W1 ;
-
+	// compute the goal vector from present position to waypoint target in meters:
+	
+	togoal.x =  goal.x  - GPSlocation.x  ;
+	togoal.y =  goal.y  - GPSlocation.y  ;
+	
+	// project the goal vector onto the direction vector between waypoints
+	// to get the distance to the "finish" line:
+	
+	temporary.WW = (	__builtin_mulss( togoal.x , goal.cosphi )
+				+ __builtin_mulss( togoal.y , goal.sinphi ))<<2 ;
+	
+	tofinish = temporary._.W1 ;
+	
 #if ( USE_CROSSTRACKING == 1 )
-
-		// project the goal vector perpendicular to the desired direction vector
-		// to get the crosstrack error
-
-		temporary.WW = ( __builtin_mulss( togoal.y , goal.cosphi )
-					   - __builtin_mulss( togoal.x , goal.sinphi ))<<2 ;
-
-		crosstrack = temporary._.W1 ;
-
-		// crosstrack is measured in meters
-		// angles are measured as an 8 bit signed character, so 90 degrees is 64 binary.
-
-		if ( crosstrack > 32 )  // more than 32 meters to the right, steer 45 degrees to the left
-		{
-			desired_dir_waypoint = goal.phi + 32 ; // 45 degrees maximum
-		}
-		else if ( crosstrack < -32 ) // more than 32 meters to the left, steer 45 degrees to the right
-		{
-			desired_dir_waypoint = goal.phi - 32 ; // -45 degress minimum
-		}
-		else  // within 32 meters of the desired track, steer in proportion to the cross track error
-		{
-			desired_dir_waypoint = goal.phi + crosstrack ;
-		}
-#else
-		desired_dir_waypoint = rect_to_polar ( & togoal ) ;
-#endif
+	
+	// project the goal vector perpendicular to the desired direction vector
+	// to get the crosstrack error
+	
+	temporary.WW = ( __builtin_mulss( togoal.y , goal.cosphi )
+				   - __builtin_mulss( togoal.x , goal.sinphi ))<<2 ;
+	
+	crosstrack = temporary._.W1 ;
+	
+	// crosstrack is measured in meters
+	// angles are measured as an 8 bit signed character, so 90 degrees is 64 binary.
+	
+	if ( crosstrack > 32 )  // more than 32 meters to the right, steer 45 degrees to the left
+	{
+		desired_dir_waypoint = goal.phi + 32 ; // 45 degrees maximum
 	}
+	else if ( crosstrack < -32 ) // more than 32 meters to the left, steer 45 degrees to the right
+	{
+		desired_dir_waypoint = goal.phi - 32 ; // -45 degress minimum
+	}
+	else  // within 32 meters of the desired track, steer in proportion to the cross track error
+	{
+		desired_dir_waypoint = goal.phi + crosstrack ;
+	}
+#else
+	desired_dir_waypoint = rect_to_polar ( & togoal ) ;
+#endif
 }
 
 
@@ -173,10 +178,24 @@ void processwaypoints(void)
 	if ( flags._.use_waypoints == 1 )
 	{
 		desired_dir = desired_dir_waypoint ;
+		
+		if (goal.legDist > 0)
+		{
+			// progress_to_goal is the fraction of the distance from the start to the finish of
+			// the current waypoint leg, that is still remaining.  it ranges from 0 - 1<<12.
+			progress_to_goal = (((long)goal.legDist - tofinish + velocity_magnitude/100)<<12) / goal.legDist ;
+			if (progress_to_goal < 0) progress_to_goal = 0 ;
+			if (progress_to_goal > (long)1<<12) progress_to_goal = (long)1<<12 ;
+		}
+		else
+		{
+			progress_to_goal = (long)1<<12 ;
+		}
 	}
 	else
 	{
 		desired_dir = bearing_to_origin ;
+		progress_to_goal = 0 ;
 	}
 	return ;
 }
