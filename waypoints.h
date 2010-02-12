72950a36b13f85535d1f737246f351e3bcf6cf15
Ben Levitt <benjie@gmail.com>
2010-02-12 02:15:02 +0000

    Allow mixing relative and absolute waypoints in one mission.
    git-svn-id: https://gentlenav.googlecode.com/svn/trunk/MatrixPilot@284 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

--- waypoints.h
+++ waypoints.h
@@ -14,12 +14,6 @@
 // Only applies if not using cross tracking.
 #define WAYPOINT_RADIUS 		25
 
-// WAYPOINT_TYPE can be either WP_ABSOLUTE or WP_RELATIVE.
-// Absoulte waypoints should have Lat and Long in degrees * 10^-7,
-// and Alt in meters above starting position (Alt same as relative).
-// Relative waypoints should have Lat, Long, and Alt in meters.
-#define WAYPOINT_TYPE			WP_RELATIVE
-
 
 ////////////////////////////////////////////////////////////////////////////////
 // Waypoint definitions
@@ -35,20 +29,23 @@
 // A waypoint is defined as { { X , Y , Z } , F }
 // where X, Y, and Z are the three coordinates of the waypoint, and F stores the flags/options for this waypoint.
 // 
-// When WAYPOINT_TYPE is set to WP_RELATIVE, 
+// Each waypoint can be defined as either a Relative waypoint or an Absolute waypoint.
+//
+// For a Relative waypoint,
 // X, Y, and Z are distances in meters, relative to the initialization location of the board.
 // X is positive as you move east
 // Y is positive as you move north
 // Z is the height
 // 
-// When WAYPOINT_TYPE is set to WP_ABSOLUTE, 
+// For an Absolute waypoint,
 // X is Logitude in degrees * 10^-7
 // Y is Latitude in degrees * 10^-7
 // Z is altitude in meters relative to the initialization location of the board.
-// As an example, the absolute waypoint { { -1219950467, 374124664, 100 }, F_NORMAL } represents a point
+// As an example, the absolute waypoint { { -1219950467, 374124664, 100 }, F_ABSOLUTE } represents a point
 // 100 meters above Baylands Park in Sunnyvale, CA, and will fly there normally (not inverted, etc.)
 // 
 // Currently F can be set to: F_NORMAL, or any combination of:
+// F_ABSOLUTE	- Waypoints are Relative by default, unless F_ABSOLUTE is specified.
 // F_INVERTED	- Navigate to this waypoint with the plane upside down. (only if STABILIZE_INVERTED_FLIGHT is set to 1 in options.h)
 // F_HOVER		- Hover the plane until reaching this waypoint. (only if STABILIZE_HOVER is set to 1 in options.h)
 //				  NOTE: while hovering, no navigation is performed, and throttle is under manual control.
