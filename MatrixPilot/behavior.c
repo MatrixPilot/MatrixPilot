287b5b564aa7fa4441f57d6fb4c169f0223e0b0c
Ben Levitt <benjie@gmail.com>
2010-06-16 16:21:45 +0000

    Improve the Copyright lines. Add Pete and Adam Barrow to the AUTHORS file.
    git-svn-id: https://gentlenav.googlecode.com/svn/trunk@423 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

--- MatrixPilot/behavior.c
+++ MatrixPilot/behavior.c
@@ -2,7 +2,7 @@
 //
 //    http://code.google.com/p/gentlenav/
 //
-// Copyright 2009, 2010
+// Copyright 2009, 2010 MatrixPilot Team
 // See the AUTHORS.TXT file for a list of authors of MatrixPilot.
 //
 // MatrixPilot is free software: you can redistribute it and/or modify
