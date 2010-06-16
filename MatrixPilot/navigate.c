714fa38eae36ef4d5b84dcb3d5212f9cd61f8a43
Ben Levitt <benjie@gmail.com>
2010-06-16 05:50:26 +0000

    Apply GPLv3 to MatrixPilot
    git-svn-id: https://gentlenav.googlecode.com/svn/trunk@421 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

--- MatrixPilot/navigate.c
+++ MatrixPilot/navigate.c
@@ -1,3 +1,24 @@
+// This file is part of MatrixPilot.
+//
+//    http://code.google.com/p/gentlenav/
+//
+// Copyright 2009, 2010
+// See the AUTHORS.TXT file for a list of authors of MatrixPilot.
+//
+// MatrixPilot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MatrixPilot is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with MatrixPilot.  If not, see <http://www.gnu.org/licenses/>.
+
+
 #include "p30f4011.h"
 #include "defines.h"
 #include "definesRmat.h"
