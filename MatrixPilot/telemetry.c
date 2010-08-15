fe903a603239e914f180f8e6c30352800b582a6b
Ben Levitt <benjie@gmail.com>
2010-08-15 18:27:46 +0000

    Improve RCON reporting via telemetry by clearing RCON after reporting it.
    git-svn-id: https://gentlenav.googlecode.com/svn/trunk@534 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

--- MatrixPilot/telemetry.c
+++ MatrixPilot/telemetry.c
@@ -287,8 +287,9 @@ void serial_output_8hz( void )
 		// The first lines of telemetry contain info about the compile-time settings from the options.h file
 		case 6:
 			serial_output("F11:WIND_EST=%i:GPS_TYPE=%i:DR=%i:BOARD_TYPE=%i:AIRFRAME=%i:RCON=%i:\r\n",
-				WIND_ESTIMATION, GPS_TYPE, DEADRECKONING, BOARD_TYPE, AIRFRAME_TYPE, RCON);
-			break;
+				WIND_ESTIMATION, GPS_TYPE, DEADRECKONING, BOARD_TYPE, AIRFRAME_TYPE, RCON) ;
+				RCON = 0 ;
+			break ;
 		case 5:
 			serial_output("F4:R_STAB=%i:P_STAB=%i:Y_STAB_R=%i:Y_STAB_A=%i:AIL_NAV=%i:RUD_NAV=%i:AH_STAB=%i:AH_WP=%i:RACE=%i:\r\n",
 				ROLL_STABILIZATION, PITCH_STABILIZATION, YAW_STABILIZATION_RUDDER, YAW_STABILIZATION_AILERON,
