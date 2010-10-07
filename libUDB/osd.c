49741f956a22b67bf01909d31049c4c837303af7
Ben Levitt <benjie@gmail.com>
2010-10-07 06:30:45 +0000

    Fix OSD test code.  It works!
    git-svn-id: https://gentlenav.googlecode.com/svn/branches/MatrixPilot_ben@605 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

--- libUDB/osd.c
+++ libUDB/osd.c
@@ -63,7 +63,7 @@ void osd_spi_write_byte(char byte)
 	OSD_SCK = 0 ;				// and CK low
 	
 	OSD_CS = 0 ;				// Set active-low CS low to start the SPI cycle 
-	spi_write_raw_byte(byte) ;		// Send the data
+	spi_write_raw_byte(byte) ;	// Send the data
 	
 	OSD_CS = 1 ;				// Set active-low CS high to end the SPI cycle 
 	
@@ -71,14 +71,14 @@ void osd_spi_write_byte(char byte)
 }
 
 
-void osd_spi_write(char address, char byte)
+void osd_spi_write(char addr, char byte)
 {
 	OSD_CS = 1 ;				// Make sure we start with active-low CS high
 	OSD_SCK = 0 ;				// and CK low
 	
 	OSD_CS = 0 ;				// Set active-low CS low to start the SPI cycle 
-	spi_write_raw_byte(address) ;	// Send the Address
-	spi_write_raw_byte(byte) ;		// Send the data
+	spi_write_raw_byte(addr) ;	// Send the Address
+	spi_write_raw_byte(byte) ;	// Send the data
 	
 	OSD_CS = 1 ;				// Set active-low CS high to end the SPI cycle 
 	
@@ -86,7 +86,7 @@ void osd_spi_write(char address, char byte)
 }
 
 
-unsigned char osd_spi_read(char address)
+unsigned char osd_spi_read(char addr)
 {
 	unsigned char SPIData = 0 ;
 	
@@ -94,7 +94,7 @@ unsigned char osd_spi_read(char address)
 	OSD_SCK = 0 ;				// and CK low
 	
 	OSD_CS = 0 ;				// Set active-low CS low to start the SPI cycle 
-	spi_write_raw_byte(address) ;	// Send the Address
+	spi_write_raw_byte(addr) ;	// Send the Address
 	OSD_MOSI = 0 ;
 	SPIData = spi_read_raw_byte() ;	// Send the data
 	
@@ -119,7 +119,7 @@ void osd_spi_write_string(const unsigned char *str)
 	
 	while (1)
 	{
-		spi_write_raw_byte(*str) ;	// Disableble auto-increment mode when sending 0xFF at the end of a string
+		osd_spi_write_byte(*str) ;	// Disableble auto-increment mode when sending 0xFF at the end of a string
 		if (*str == 0xFF) break;
 		str++;
 	}
@@ -129,24 +129,24 @@ void osd_spi_write_string(const unsigned char *str)
 void osd_spi_write_raw_uint(unsigned int val)
 {
 	int d = (val / 10000) ;
-	spi_write_raw_byte((d) ? d : 0x0A) ;
+	osd_spi_write_byte((d) ? d : 0x0A) ;
 	val -= d*10000 ;
 	
 	d = (val / 1000) ;
-	spi_write_raw_byte((d) ? d : 0x0A) ;
+	osd_spi_write_byte((d) ? d : 0x0A) ;
 	val -= d*1000 ;
 	
 	d = (val / 100) ;
-	spi_write_raw_byte((d) ? d : 0x0A) ;
+	osd_spi_write_byte((d) ? d : 0x0A) ;
 	val -= d*100 ;
 	
 	d = (val / 10) ;
-	spi_write_raw_byte((d) ? d : 0x0A) ;
+	osd_spi_write_byte((d) ? d : 0x0A) ;
 	val -= d*10 ;
 	
-	spi_write_raw_byte((val) ? val : 0x0A) ;
+	osd_spi_write_byte((val) ? val : 0x0A) ;
 	
-	spi_write_raw_byte(0xFF) ;		// Disableble auto-increment mode
+	osd_spi_write_byte(0xFF) ;		// Disableble auto-increment mode
 }
 
 
@@ -155,7 +155,7 @@ void osd_spi_write_int(int val)
 	osd_spi_write(0x04,1) ;		// DMM: Enable auto-increment mode
 	
 	if (val < 0)
-		spi_write_raw_byte(0x49) ;	// '-'
+		osd_spi_write_byte(0x49) ;	// '-'
 	
 	osd_spi_write_raw_uint(abs(val)) ;
 }
@@ -190,8 +190,8 @@ void osd_countdown(int countdown)
 	{
 		// automatic black level control, have to read, augment and rewrite
 		// The data sheet is rather specific about this
-		x = osd_spi_read(0xEC) ;	// OSDBD
-		x &= 0xEF ;
+		//x = osd_spi_read(0xEC) ;	// OSDBD
+		//x &= 0xEF ;
 	}
 	else if (countdown == 948)
 	{
@@ -203,60 +203,19 @@ void osd_countdown(int countdown)
 	}
 	else if (countdown == 946)
 	{
-		osd_spi_write(0, 0x00) ;	// VM0: disable display of OSD image
+		osd_spi_write(0, 0x08) ;	// VM0: enable display of OSD image
 		
-		osd_spi_write_location(9, 12) ;
+		osd_spi_write_location(9, 11) ;
 		osd_spi_write_string(sample) ;
-		
-		/*
-		x = 25 ;
-		
-		osd_spi_write(0x05, 0x01) ;	// DMAH
-		
-		osd_spi_write(0x06, x++) ;	// DMAL
-		osd_spi_write(0x07, 0x1D) ;
-		
-		osd_spi_write(0x06, x++) ;	// DMAL
-		osd_spi_write(0x07, 0x0B) ;
-		
-		osd_spi_write(0x06, x++) ;	// DMAL
-		osd_spi_write(0x07, 0x17) ;
-		
-		osd_spi_write(0x06, x++) ;	// DMAL
-		osd_spi_write(0x07, 0x1A) ;
-		
-		osd_spi_write(0x06, x++) ;	// DMAL
-		osd_spi_write(0x07, 0x16) ;
-		
-		osd_spi_write(0x06, x++) ;	// DMAL
-		osd_spi_write(0x07, 0x0F) ;
-		*/
-		
-		osd_spi_write(0, 0x0C) ;	// enable display of OSD image at next VSYNC
 	}
-	else {
-		osd_spi_write(0, 0x00) ;	// VM0: disable display of OSD image
+	else if (countdown < 940) {
+		osd_spi_write(0, 0x08) ;	// VM0: enable display of OSD image
 		
-		osd_spi_write_location(0, 3) ;
-		osd_spi_write_int(countdown) ;
 		
-		osd_spi_write_location(0, 22) ;
+		osd_spi_write_location(6, 18) ;
 		osd_spi_write_int(calculated_heading) ;
 		
-		osd_spi_write(0, 0x0C) ;	// enable display of OSD image at next VSYNC
+		osd_spi_write_location(6, 7) ;
+		osd_spi_write_int(countdown) ;
 	}
 }
-
-
-/*
-int main (void)
-{
-	while(1)
-	{
-		osd_spi_write(0, 0x08);	//enable display of OSD image
-		delay_ms(1000);
-		osd_spi_write(0, 0);	//disable display of OSD image
-		delay_ms(1000);
-	}
-}
-*/
