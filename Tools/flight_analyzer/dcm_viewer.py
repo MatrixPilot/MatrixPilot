ba5a95cefc5b3258eb90800a3e1a093fb1362b71
Pete Hollands <peter.hollands@gmail.com>
2010-11-12 20:25:56 +0000

    dcm_viewer.py: Small improvements in display.
    git-svn-id: https://gentlenav.googlecode.com/svn/trunk@677 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

--- Tools/flight_analyzer/dcm_viewer.py
+++ Tools/flight_analyzer/dcm_viewer.py
@@ -14,7 +14,7 @@ from matrixpilot_lib import telemetry
 from matrixpilot_lib import matrix_multiply_3x3_3x1
 
 def conv_udb_vpython(vector) :
-    """Convert a vector from UDB coordinate reference to vpython refernce"""
+    """Convert a vector from UDB coordinate reference to vpython reference"""
     # conversion from UDB coordinates to V python coordinates.
     # X axis is out of left wing in both coordinate systems.
     # Y is straight up in vpython, and is the negative of the UDB's Z axis,
@@ -36,32 +36,36 @@ telemetry_line = telemetry() # instantiate the telemetry class
 max_tm_actual = 0
 line_no = 0
 
-fuselage_vector_udb = (0, 16384, 0) # fuselage along Y axis of UDBcoordinate reference
+f_length = 50
+fuselage_vector_udb = (0, 1, 0) # fuselage along Y axis of UDBcoordinate reference
 fuselage_vector_vpython  = conv_udb_vpython(fuselage_vector_udb)
 fuselage_plane = arrow(pos=(0, 0, 0), axis = fuselage_vector_vpython,
-                       color  = color.white,  shaftwidth=1,length = 10000 )
+                       color  = color.white, length = f_length)
 
-left_wing_vector_udb = (14000,0, 0)
+w_length = 50
+left_wing_vector_udb = (1,0, 0)
 left_wing_vector_vpython = conv_udb_vpython(left_wing_vector_udb)
 left_wing      = arrow(pos=(0, 0, 0), axis = left_wing_vector_vpython,
-                       color = color.orange, shaftwidth =1, length = 10000)
+                       color = color.orange, length = w_length)
 
-right_wing_vector_udb = (-14000,0,0)
+right_wing_vector_udb = (-1,0,0)
 right_wing_vector_vpython = conv_udb_vpython(right_wing_vector_udb)
 right_wing     = arrow(pos=(0, 0, 0), axis = right_wing_vector_vpython,
-                       color = color.orange, shaftwidth =1, length =7000)
+                       color = color.orange, length = w_length)
 
-x_axis = arrow(pos=(0,0,0), axis=(16384,    0,     0), color = color.green, shaftwidth=1,length = 16384)
-y_axis = arrow(pos=(0,0,0), axis=(    0, -16384,     0), color = color.red,   shaftwidth=1,length = 16384)
-z_axis = arrow(pos=(0,0,0), axis=(    0,    0,16384), color = color.blue,  shaftwidth=1,length = 16384)
+x_axis = arrow(pos=(0,0,0), axis=(1,    0,   0), color = color.green, shaftwidth=1,length = 100)
+y_axis = arrow(pos=(0,0,0), axis=( 0,  -1,   0), color = color.red,   shaftwidth=1,length = 100)
+z_axis = arrow(pos=(0,0,0), axis=( 0,    0,  1), color = color.blue,  shaftwidth=1,length = 100)
 rmat = [0,0,0,0,0,0,0,0,0] 
 result = "Not set yet"
-while (1) :
+while (True) :
     try:
         line_no += 1
         line = ser.readline()
-        print line
+        
         result = telemetry_line.parse(line, line_no, max_tm_actual)
+        print line
+        
         if result == "F2" : # If received SERIAL_UDB or SERIAL_UDB_EXTRA format telemetry
             rmat[0] =   telemetry_line.rmat0 /16384.0
             rmat[1] =   telemetry_line.rmat1 /16384.0
@@ -74,10 +78,16 @@ while (1) :
             rmat[8] =   telemetry_line.rmat8 /16384.0
             fuselage_vector_rotated  = matrix_multiply_3x3_3x1(rmat,fuselage_vector_udb)
             fuselage_plane.axis  = conv_udb_vpython(fuselage_vector_rotated)
+            fuselage_plane.length = f_length
+            fuselage_plane.pos = (0,0,0)
             left_wing_vector_rotated = matrix_multiply_3x3_3x1(rmat,left_wing_vector_udb)
             left_wing.axis       = conv_udb_vpython(left_wing_vector_rotated)
+            left_wing.length = w_length
+            left_wing.pos = (0,0,0)
             right_wing_vector_rotated = matrix_multiply_3x3_3x1(rmat,right_wing_vector_udb)
             right_wing.axis       = conv_udb_vpython(right_wing_vector_rotated)
+            right_wing.length = w_length
+            right_wing.pos = (0,0,0)
     except:
         print "There has been a program exception."
         print "This has been caught by dcm_viewer's own code logic"
