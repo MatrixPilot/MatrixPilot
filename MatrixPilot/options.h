64e9c4885cb68b3a99f26aeeed2a55e11ae31a87
Ben Levitt <benjie@gmail.com>
2011-02-13 10:58:40 +0000

    Update options.h files to include a bit more description of the different boards.
    git-svn-id: https://gentlenav.googlecode.com/svn/trunk@797 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

--- MatrixPilot/options.h
+++ MatrixPilot/options.h
@@ -30,6 +30,10 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 // Set Up Board Type (Set to RED_BOARD, GREEN_BOARD, UDB3_BOARD, RUSTYS_BOARD, or UDB4_BOARD)
+// GREEN_BOARD - Board is green and includes 2 vertical gyro daugter-boards.
+// RED_BOARD   - Board is red, and includes 2 vertical gyro daugter-boards.
+// UDB3_BOARD  - Board is red, and includes a single, flat, multi-gyro daugter-board.
+// See the MatrixPilot wiki for more details on different UDB boards.
 // If building for UDB4, use the MatrixPilot-udb4.mcp project file.
 #define BOARD_TYPE 							RED_BOARD
 
