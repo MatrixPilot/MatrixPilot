2fff6fe917e03ee46d86c1d12fdfd046bd7874bd
Pete Hollands <peter.hollands@gmail.com>
2011-04-09 14:45:01 +0000

    ADD Clock type to initial telemetry print out (CRYSTAL or FRC8X)
    git-svn-id: https://gentlenav.googlecode.com/svn/trunk@850 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

--- MatrixPilot/telemetry.c
+++ MatrixPilot/telemetry.c
@@ -410,8 +410,9 @@ void serial_output_8hz( void )
 				// if there was not a software reset (trap error) clear the trap data
 				trap_flags = trap_source = osc_fail_count = 0 ;
 			}
-			serial_output("\r\nF14:WIND_EST=%i:GPS_TYPE=%i:DR=%i:BOARD_TYPE=%i:AIRFRAME=%i:RCON=0x%X:TRAP_FLAGS=0x%X:TRAP_SOURCE=0x%X:ALARMS=%i:\r\n",
-				WIND_ESTIMATION, GPS_TYPE, DEADRECKONING, BOARD_TYPE, AIRFRAME_TYPE, RCON , trap_flags , trap_source , osc_fail_count ) ;
+			serial_output("\r\nF14:WIND_EST=%i:GPS_TYPE=%i:DR=%i:BOARD_TYPE=%i:AIRFRAME=%i:RCON=0x%X:TRAP_FLAGS=0x%X:TRAP_SOURCE=0x%X:ALARMS=%i:"  \
+							"CLOCK=%i:\r\n",
+				WIND_ESTIMATION, GPS_TYPE, DEADRECKONING, BOARD_TYPE, AIRFRAME_TYPE, RCON , trap_flags , trap_source , osc_fail_count, CLOCK_CONFIG ) ;
 				RCON = 0 ;
 				trap_flags = 0 ;
 				trap_source = 0 ;
