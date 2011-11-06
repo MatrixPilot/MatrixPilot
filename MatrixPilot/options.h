286a523100eea223056825491b9d8e93c5ad7897
Pete Hollands <peter.hollands@gmail.com>
2011-11-06 15:34:56 +0000

    Add UDB4 board type to options.h (Thanks to Phil Kocmoud for pointing out that it was missing).
    git-svn-id: https://gentlenav.googlecode.com/svn/trunk@1079 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

--- MatrixPilot/options.h
+++ MatrixPilot/options.h
@@ -36,8 +36,9 @@
 // GREEN_BOARD - Board is green and includes 2 vertical gyro daugter-boards.
 // RED_BOARD   - Board is red, and includes 2 vertical gyro daugter-boards.
 // UDB3_BOARD  - Board is red, and includes a single, flat, multi-gyro daugter-board.
+// UDB4_BOARD  - Board is red, has 8 inputs, 8 output and no gyro daughter-board.
 // See the MatrixPilot wiki for more details on different UDB boards.
-// If building for UDB4, use the MatrixPilot-udb4.mcp project file.
+// If building for the UDB4, use the MatrixPilot-udb4.mcw project workspace. 
 #define BOARD_TYPE 							UDB3_BOARD
 
 
