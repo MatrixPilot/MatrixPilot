51802d2e85907f968d375ecc8652aa0f55e8864b
Pete Hollands <peter.hollands@gmail.com>
2012-04-13 20:48:33 +0000

    Tools: MAVLink: Fix gen_MatrixPilot.py so that preceding C files from previous generation processes are all deleted  correctly before new C files are created.
    git-svn-id: https://gentlenav.googlecode.com/svn/trunk@1410 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

--- Tools/MAVLink/pymavlink/generator/gen_MatrixPilot.py
+++ Tools/MAVLink/pymavlink/generator/gen_MatrixPilot.py
@@ -60,33 +60,50 @@ def copy_xml_files(source_directory,target_directory):
         print "Copying ...", basename
         copy(afile, target_directory)
 
+
+########### Generate MAVlink files for C and Python from XML definitions
 protocol = "1.0"
  
 xml_directory = './message_definitions/v'+protocol
 print "xml_directory is", xml_directory
+
 xml_file_names = []
+xml_file_names.append(xml_directory+"/"+"common.xml")
 xml_file_names.append(xml_directory+"/"+"matrixpilot.xml")
 
 for xml_file in xml_file_names:
-    print "xml file is ", xml_file
+    print "xml file is ", xml_file   
+    
+    xml_file_base = os.path.basename(xml_file)
+    xml_file_base = re.sub("\.xml","", xml_file_base)
+    print "xml_file_base is", xml_file_base
+    target_directory = "../../../../MAVLink/include/"+xml_file_base
+    source_directory = "C/include_v"+protocol+"/"+xml_file_base
+
+    print "About to remove all files in",source_directory
+    print "OK to continue ?[Yes / No]: ",
+    line = sys.stdin.readline()
+    if line == "Yes\n" or line == "yes\n" \
+       or line == "Y\n" or line == "y\n":
+        print "passed"
+        remove_include_files(source_directory)
+        print "Finished removing C include files for", xml_file_base
+    else :
+        print "Your answer is No. Exiting Program"
+        sys.exit()
+
     opts = options(lang = "C", output = "C/include_v"+protocol, \
                    wire_protocol=protocol)
     args = []
     args.append(xml_file)
+    print "About to generate C include files"
     mavgen(opts, args)
-    xml_file_base = os.path.basename(xml_file)
-    xml_file_base = re.sub("\.xml","", xml_file_base)
-    print "xml_file_base is", xml_file_base
     opts = options(lang = "python", \
                    output="python/mavlink_"+xml_file_base+"_v"+protocol+".py", \
                    wire_protocol=protocol)
+    print "About to generate python parsers"
     mavgen(opts,args)
-
-mavlink_directory_list = ["common","matrixpilot"]
-for mavlink_directory in mavlink_directory_list :
-    # Look specifically for MatrixPilot directory structure
-    target_directory = "../../../../MAVLink/include/"+mavlink_directory
-    source_directory = "C/include_v"+protocol+"/"+mavlink_directory
+    
     if os.access(source_directory, os.R_OK):
         if os.access(target_directory, os.W_OK):
             print "Preparing to copy over files..."
@@ -109,7 +126,10 @@ for mavlink_directory in mavlink_directory_list :
         print "Could not find files to copy at", source_directory
         print "Exiting Program."
         sys.exit()
+        
+##### End of Main program to generate MAVLink C and Python files ####
 
+##### Copy new XML message definitions to main trunk directories
 source_directory = "message_definitions/V1.0"
 target_directory = "../../../../MAVLink/message_definitions"
 if os.access(source_directory, os.R_OK):
@@ -139,13 +159,7 @@ else:
     print "Could not find files to copy at", source_directory
     print "Exiting Program."
     sys.exit()
+print "Program has finished, please press Return Key to exit"
+line = sys.stdin.readline()
 
-
-        
-        
-
-        
-            
-
-    
 
