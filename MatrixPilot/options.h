5a61e22c09718fa44ed34e2d5bdca11085c3d8c9
William Premerlani <wpremerlani@gmail.com>
2012-04-30 22:48:34 +0000

    MatrixPilot: Acceleration research: reset the options to normal default values in preparation for release for testing
    git-svn-id: https://gentlenav.googlecode.com/svn/branches/MatrixPilot_AccelerationCompensation@1466 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

--- MatrixPilot/options.h
+++ MatrixPilot/options.h
@@ -321,7 +321,7 @@
 // SERIAL_MAVLINK is only supported on the UDB4 to ensure that sufficient RAM is available.
 // Note that SERIAL_MAVLINK defaults to using a baud rate of 57600 baud (other formats default to 19200)
 
-#define SERIAL_OUTPUT_FORMAT 	SERIAL_NONE
+#define SERIAL_OUTPUT_FORMAT 	SERIAL_UDB_EXTRA
 
 // MAVLink requires an aircraft Identifier (I.D) as it is deaigned to control multiple aircraft
 // Each aircraft in the sky will need a unique I.D. in the range from 0-255
