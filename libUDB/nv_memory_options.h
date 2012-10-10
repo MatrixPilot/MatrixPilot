99483277ff5a22a53d74ad91cd38412442106c6a
Ben Levitt <benjie@gmail.com>
2012-10-10 16:37:12 +0000

    Remove unused advanced options to avoid user problems
    git-svn-id: https://gentlenav.googlecode.com/svn/trunk@1742 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

--- libUDB/nv_memory_options.h
+++ libUDB/nv_memory_options.h
@@ -10,12 +10,6 @@
 
 
 ////////////////////////////////////////////////////////////////////////////////
-// Use I2C port driver timeout
-// Set to 1 to use and 0 to not use
-#define USE_I2C_TIMEOUT					0
-
-
-////////////////////////////////////////////////////////////////////////////////
 // Use non volatile memory to store and recall settings
 // Set to 1 to include non volatile memory service in compilation
 // WARNING: Use on UDB4+ only
@@ -30,13 +24,6 @@
 // Reset to 0 after clearing to use the non volatile memory again.
 #define MANUAL_ERASE_TABLE				0
 
-
-////////////////////////////////////////////////////////////////////////////////
-// Use flexifunctions for mixing instead of standard servomix
-// Setting to 1 to enable, 0 to use standard servomix.
-// WARNING: THIS FEATURE IS UNDER DEVELOPMENT
-// WARNING: Must use MAVlink and NV memory on UDB4+ only
-#define USE_FLEXIFUNCTION_MIXING		0
 
 ////////////////////////////////////////////////////////////////////////////////
 // Use variable data width in HILSIM for output channels
