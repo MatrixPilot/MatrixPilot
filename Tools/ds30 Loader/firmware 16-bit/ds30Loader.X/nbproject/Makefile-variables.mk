258bbb2f45a6273defc6d313f03dc5d8526c22eb
Robert Dickenson <robert.dickenson@gmail.com>
2013-02-09 00:14:46 +0000

    MatrixPilot trunk: updated ds30-loader with latest changes by the author, Mikael Gustavsson. (Note: latest versions of windows software not updated since the GUI loader, version 1.5.5, did not work on my Windows 7 64-bit, hangs on startup)
    git-svn-id: https://gentlenav.googlecode.com/svn/trunk@2066 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

---
+++ Tools/ds30 Loader/firmware 16-bit/ds30Loader.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=ds30Loader.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/ds30Loader.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=ds30loader.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/ds30loader.x.tar
