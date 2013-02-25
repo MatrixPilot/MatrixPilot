b0a7348e2fdaa1698ea6b6ff34611ebe9a0032d1
Ben Levitt <benjie@gmail.com>
2013-02-25 07:05:52 +0000

    trunk: Consistently default HILSIM UDP port to 14551 and fix typo in options.h and update prebuilt mac plugin, and update win32 plugin to the version built in VC++ (now the right file size).
    git-svn-id: https://gentlenav.googlecode.com/svn/trunk@2177 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

--- MatrixPilot/options.h
+++ MatrixPilot/options.h
@@ -622,10 +622,10 @@
 // passing RC inputs through to the simulated UDB.
 #define SILSIM								0
 #define SILSIM_GPS_RUN_AS_SERVER			0
-#define SILSIM_GPS_PORT						14551		// default port to connect to QGroundControl
+#define SILSIM_GPS_PORT						14551		// default port to connect to XPlane HILSIM plugin
 #define SILSIM_GPS_HOST						"127.0.0.1"
 #define SILSIM_TELEMETRY_RUN_AS_SERVER		0
-#define SILSIM_TELEMETRY_PORT				14550		// default port to connect to XPlane HILSIM plugin
+#define SILSIM_TELEMETRY_PORT				14550		// default port to connect to QGroundControl
 #define SILSIM_TELEMETRY_HOST				"127.0.0.1"
 #define SILSIM_SERIAL_RC_INPUT_DEVICE		""			// i.e. "COM4" or "/dev/cu.usbserial-A600dP4v", or "" to disable
 #define SILSIM_SERIAL_RC_INPUT_BAUD			38400
