181de9063eb1a3fda58cf51d3ad7c1e1a7892d47
Robert Dickenson <robert.dickenson@gmail.com>
2013-03-12 20:37:06 +0000

    MatrixPilot_UDB5 branch: changing the traps module back to .s
    git-svn-id: https://gentlenav.googlecode.com/svn/branches/MatrixPilot_UDB5@2243 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

--- libUDB/traps_udb_asm.s
+++ libUDB/traps_udb_asm.s
@@ -1,18 +1,30 @@
 
+.ifdef __dsPIC33F
+.include "p33Fxxxx.inc"
+.endif
+.ifdef __dsPIC30F
 .include "p30fxxxx.inc"
+.endif
+.ifdef __dsPIC33E
+.include "p33Exxxx.inc"
+.endif
 
 ; MATH_ERROR 1
 ; STACK_ERROR 2
 ; ADDRESS_ERROR 4
 ; OSC_FAIL 8
-; unhandled interrupt 16 
+; unhandled interrupt 16
+; DMACError 32
 
 .extern	_trap_flags
 .extern _trap_source
 .extern _osc_fail_count
 
+.ifndef __dsPIC33E
 __MathError:	mov.w #1,w0
 				bra save_states
+.else
+.endif
 
 __StackError:	mov.w #2,w0
 				bra save_states
@@ -20,10 +32,20 @@ __StackError:	mov.w #2,w0
 __AddressError:	mov.w #4,w0
 				bra save_states
 
-__OscillatorFail: btss OSCCON , #CF
+__OscillatorFail:
+.ifdef __dsPIC30F
+				btss OSCCON , #CF
 				bra FalseAlarm
+.else
+				inc _osc_fail_count
+.endif
 				mov.w #8,w0
 				bra save_states
+
+.ifndef __dsPIC30F
+__DMACError:	mov.w #32,w0
+				bra save_states
+.endif
 
 __DefaultInterrupt:	mov.w #16,w0
 				bra	save_states
@@ -35,14 +57,19 @@ save_states:	mov.w w0,_trap_flags
 				mov.w w1, _trap_source+2
 				reset
 
+.ifdef __dsPIC30F
 FalseAlarm:		bclr.b	INTCON1, #OSCFAIL
 				inc		_osc_fail_count
 				retfie
+.endif
 
 .global __MathError
 .global __StackError
 .global __AddressError
 .global __OscillatorFail
 .global __DefaultInterrupt
+.ifndef __dsPIC30F
+.global __DMACError
+.endif
 
 
