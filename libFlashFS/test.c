51392ecadbd323d0c5941be30886cf8c20f3ab04
Robert Dickenson <robert.dickenson@gmail.com>
2013-04-14 03:55:16 +0000

    MatrixPilot_AUAV3 branch: added modules for ini file support, with test routine
    git-svn-id: https://gentlenav.googlecode.com/svn/branches/MatrixPilot_AUAV3@2381 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

---
+++ libFlashFS/test.c
@@ -0,0 +1,117 @@
+/* Simple test program
+ *
+ *  gcc -o test test.c minIni.c
+ */
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include "minIni.h"
+
+#define sizearray(a)  (sizeof(a) / sizeof((a)[0]))
+
+const char inifile[] = "test.ini";
+const char inifile2[] = "testplain.ini";
+
+int Callback(const char *section, const char *key, const char *value, const void *userdata)
+{
+  (void)userdata; /* this parameter is not used in this example */
+  printf("    [%s]\t%s=%s\n", section, key, value);
+  return 1;
+}
+
+void _assert(int foo)
+{
+}
+
+//int main(void)
+int test_ini(void)
+{
+  char str[100];
+  long n;
+  int s, k;
+  char section[50];
+
+  /* string reading */
+  n = ini_gets("first", "string", "dummy", str, sizearray(str), inifile);
+  _assert(n==4 && strcmp(str,"noot")==0);
+  n = ini_gets("second", "string", "dummy", str, sizearray(str), inifile);
+  _assert(n==4 && strcmp(str,"mies")==0);
+  n = ini_gets("first", "undefined", "dummy", str, sizearray(str), inifile);
+  _assert(n==5 && strcmp(str,"dummy")==0);
+  /* ----- */
+  n = ini_gets("", "string", "dummy", str, sizearray(str), inifile2);
+  _assert(n==4 && strcmp(str,"noot")==0);
+  n = ini_gets(NULL, "string", "dummy", str, sizearray(str), inifile2);
+  _assert(n==4 && strcmp(str,"noot")==0);
+  /* ----- */
+  printf("1. String reading tests passed\n");
+
+  /* value reading */
+  n = ini_getl("first", "val", -1, inifile);
+  _assert(n==1);
+  n = ini_getl("second", "val", -1, inifile);
+  _assert(n==2);
+  n = ini_getl("first", "undefined", -1, inifile);
+  _assert(n==-1);
+  /* ----- */
+  n = ini_getl(NULL, "val", -1, inifile2);
+  _assert(n==1);
+  /* ----- */
+  printf("2. Value reading tests passed\n");
+
+  /* string writing */
+  n = ini_puts("first", "alt", "flagged as \"correct\"", inifile);
+  _assert(n==1);
+  n = ini_gets("first", "alt", "dummy", str, sizearray(str), inifile);
+  _assert(n==20 && strcmp(str,"flagged as \"correct\"")==0);
+  /* ----- */
+  n = ini_puts("second", "alt", "correct", inifile);
+  _assert(n==1);
+  n = ini_gets("second", "alt", "dummy", str, sizearray(str), inifile);
+  _assert(n==7 && strcmp(str,"correct")==0);
+  /* ----- */
+  n = ini_puts("third", "test", "correct", inifile);
+  _assert(n==1);
+  n = ini_gets("third", "test", "dummy", str, sizearray(str), inifile);
+  _assert(n==7 && strcmp(str,"correct")==0);
+  /* ----- */
+  n = ini_puts("second", "alt", "overwrite", inifile);
+  _assert(n==1);
+  n = ini_gets("second", "alt", "dummy", str, sizearray(str), inifile);
+  _assert(n==9 && strcmp(str,"overwrite")==0);
+  /* ----- */
+  n = ini_puts(NULL, "alt", "correct", inifile2);
+  _assert(n==1);
+  n = ini_gets(NULL, "alt", "dummy", str, sizearray(str), inifile2);
+  _assert(n==7 && strcmp(str,"correct")==0);
+  /* ----- */
+  printf("3. String writing tests passed\n");
+
+  /* section/key enumeration */
+  printf("4. Section/key enumertion, file contents follows\n");
+  for (s = 0; ini_getsection(s, section, sizearray(section), inifile) > 0; s++) {
+    printf("    [%s]\n", section);
+    for (k = 0; ini_getkey(section, k, str, sizearray(str), inifile) > 0; k++) {
+      printf("\t%s\n", str);
+    } /* for */
+  } /* for */
+
+  /* browsing through the file */
+  printf("5. browse through all settings, file contents follows\n");
+  ini_browse(Callback, NULL, inifile);
+
+  /* string deletion */
+  n = ini_puts("first", "alt", NULL, inifile);
+  _assert(n==1);
+  n = ini_puts("second", "alt", NULL, inifile);
+  _assert(n==1);
+  n = ini_puts("third", NULL, NULL, inifile);
+  _assert(n==1);
+  /* ----- */
+  n = ini_puts(NULL, "alt", NULL, inifile2);
+  _assert(n==1);
+  printf("6. String deletion tests passed\n");
+
+  return 0;
+}
+
