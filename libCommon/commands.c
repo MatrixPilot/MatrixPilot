20f343716c8ada335d56c9be43968cb637bb42e7
Robert Dickenson <robert.dickenson@gmail.com>
2013-05-12 14:49:39 +0000

    MatrixPilot_trunk: fixed AUAV3 MPLAB-8 project and added support files for debug port.
    git-svn-id: https://gentlenav.googlecode.com/svn/trunk@2519 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

---
+++ libCommon/commands.c
@@ -0,0 +1,282 @@
+// This file is part of MatrixPilot.
+//
+//    http://code.google.com/p/gentlenav/
+//
+// Copyright 2009-2011 MatrixPilot Team
+// See the AUTHORS.TXT file for a list of authors of MatrixPilot.
+//
+// MatrixPilot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MatrixPilot is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with MatrixPilot.  If not, see <http://www.gnu.org/licenses/>.
+
+
+#include "../libUDB/libUDB.h"
+#include "../libUDB/interrupt.h"
+//#include "config.h"
+//#include "redef.h"
+#include "../libDCM/estAltitude.h"
+//#include "defines.h"
+//#include "p33Exxxx.h"
+#include "../libCommon/uart3.h"
+#include <string.h>
+#include <stdio.h>
+
+
+#define LOWORD(a) ((WORD)(a))
+#define HIWORD(a) ((WORD)(((DWORD)(a) >> 16) & 0xFFFF))
+
+void AT45D_FormatFS(void);
+
+typedef struct tagCmds {
+	int index;
+	void (*fptr)(void);
+	const char const * cmdstr;
+} cmds_t;
+
+
+int logging_enabled = 0;
+int cmdlen = 0;
+char cmdstr[32];
+
+
+void cmd_ver(void)
+{
+	printf("AUAV3 v0.1, " __TIME__ " " __DATE__ "\r\n");
+}
+
+void cmd_start(void)
+{
+	printf("starting.\r\n");
+	logging_enabled = 1;
+}
+
+void cmd_stop(void)
+{
+	printf("stopped.\r\n");
+	logging_enabled = 0;
+}
+
+void cmd_on(void)
+{
+	printf("on.\r\n");
+	SRbits.IPL = 0 ;	// turn on all interrupt priorities
+}
+
+void cmd_off(void)
+{
+	printf("off.\r\n");
+	SRbits.IPL = 7 ;	// turn off all interrupt priorities
+}
+
+void cmd_cpuload(void)
+{
+	printf("CPU Load %u%%\r\n", udb_cpu_load());
+}
+
+void cmd_crash(void)
+{
+	static int i;
+	char buffer[32];
+
+	sprintf(buffer, "overflowing stack %u.\r\n", i++);
+	printf(buffer);
+	cmd_crash();
+}
+
+void cmd_adc(void)
+{
+//	printf("ADC vcc %u, 5v %u, rssi %u\r\n", udb_vcc.value, udb_5v.value, udb_rssi.value);
+}
+
+void cmd_barom(void)
+{
+ 	printf("Barometer temp %i, pres %u, alt %u, agl %u\r\n",
+         get_barometer_temperature(),
+         (uint16_t)get_barometer_pressure(),
+         (uint16_t)get_barometer_altitude(),
+         (uint16_t)get_barometer_agl_altitude());
+}
+
+void cmd_magno(void)
+{
+}
+
+void printbin16(int a)
+{
+	unsigned int i;
+	for (i = 0x8000; i > 0; i >>= 1) {
+		if (a & i) printf("1");
+		else printf("0");
+	}
+}
+
+const char *byte_to_binary(int x)
+{
+    static char b[9];
+    int z;
+
+    b[0] = '\0';
+    for (z = 128; z > 0; z >>= 1) {
+        strcat(b, ((x & z) == z) ? "1" : "0");
+    }
+    return b;
+}
+
+const char *word_to_binary(int x)
+{
+    static char b[17];
+    unsigned int z;
+
+    b[0] = '\0';
+    for (z = 0x8000; z > 0; z >>= 1) {
+        strcat(b, ((x & z) == z) ? "1" : "0");
+    }
+    return b;
+}
+
+void show_size_msgDataParse(void);
+
+void gentrap(void);
+
+void cmd_trap(void)
+{
+	gentrap();
+}
+
+void cmd_reg(void)
+{
+	printf("USB Registers:\r\n");
+	printf("\tU1OTGSTAT = %s\r\n", word_to_binary(U1OTGSTAT));
+	printf("\tU1OTGCON  = %s\r\n", word_to_binary(U1OTGCON));
+	printf("\tU1STAT    = %s\r\n", word_to_binary(U1STAT));
+	printf("\tU1CON     = %s\r\n", word_to_binary(U1CON));
+	printf("\tU1CNFG1   = %s\r\n", word_to_binary(U1CNFG1));
+	printf("\tU1CNFG2   = %s\r\n", word_to_binary(U1CNFG2));
+	printf("\tU1OTGIR   = %s\r\n", word_to_binary(U1OTGIR));
+	printf("\tU1OTGIE   = %s\r\n", word_to_binary(U1OTGIE));
+
+	printf("IC Registers:\r\n");
+	printf("\tIC1CON1 = %s %04x\r\n", word_to_binary(IC1CON1), IC1CON1);
+	printf("\tIC1CON2 = %s %04x\r\n", word_to_binary(IC1CON2), IC1CON2);
+	printf("\tIC2CON1 = %s %04x\r\n", word_to_binary(IC2CON1), IC2CON1);
+	printf("\tIC2CON2 = %s %04x\r\n", word_to_binary(IC2CON2), IC2CON2);
+
+/*
+UxOTGSTAT: USB OTG STATUS REGISTER
+VBUSVD: A-VBUS Valid Indicator bit
+1 = The VBUS voltage is above VA_VBUS_VLD (as defined in the USB OTG Specification) on the A device
+0 = The VBUS voltage is below VA_VBUS_VLD on the A device
+
+
+UxOTGCON: USB ON-THE-GO CONTROL REGISTER
+bit 3 VBUSON: VBUS Power-on bit(1)
+1 = VBUS line is powered
+0 = VBUS line is not powered
+
+UxCNFG2: USB CONFIGURATION REGISTER 2
+ */
+}
+
+#if (RECORD_FREE_STACK_SPACE == 1)
+extern uint16_t maxstack;
+#endif
+
+void cmd_stack(void)
+{
+#if (RECORD_FREE_STACK_SPACE == 1)
+	printf("maxstack %x\r\n", maxstack);
+	printf("SP_start %x\r\n", SP_start());
+	printf("SP_limit %x\r\n", SP_limit());
+	printf("SP_current %x\r\n", SP_current());
+	printf("stack usage %u\r\n", maxstack - SP_start());
+#else
+	printf("stack reporting disabled.\r\n");
+#endif
+}
+
+void cmd_reset(void)
+{
+	asm("reset");
+}
+
+void cmd_help(void);
+
+const cmds_t cmdslist[] = {
+	{ 0, cmd_help,   "help" },
+	{ 0, cmd_ver,    "ver" },
+	{ 0, cmd_start,  "start" },
+	{ 0, cmd_stop,   "stop" },
+	{ 0, cmd_on,     "on" },
+	{ 0, cmd_off,    "off" },
+	{ 0, cmd_stack,  "stack" },
+	{ 0, cmd_reg,    "reg" },
+	{ 0, cmd_adc,    "adc" },
+	{ 0, cmd_barom,  "bar" },
+	{ 0, cmd_cpuload,"cpu" },
+	{ 0, cmd_magno,  "mag" },
+	{ 0, cmd_crash,  "crash" },
+	{ 0, cmd_reset,  "reset" },
+	{ 0, cmd_trap,   "trap" },
+};
+
+void cmd_help(void)
+{
+	int i;
+
+	printf("Commands:\r\n");
+	for (i = 0; i < (sizeof(cmdslist)/sizeof(cmdslist[0])); i++) {
+		printf("\t%s\r\n", cmdslist[i].cmdstr);
+	}
+}
+
+// int strncmp(const char *string1, const char *string2, size_t count);
+// int strcmp(const char *string1, const char *string2);
+
+void command(char* cmdstr)
+{
+	int i;
+
+	for (i = 0; i < (sizeof(cmdslist)/sizeof(cmdslist[0])); i++) {
+//		printf("comparing %s with %s\r\n", cmdstr, cmdlist[i]);
+		if (strcmp(cmdslist[i].cmdstr, cmdstr) == 0) {
+//			printf("found command %u (%s)\r\n", i, cmdslist[i].cmdstr);
+			cmdslist[i].fptr();
+		}
+	}
+}
+
+void console(void)
+{
+    if (UART3IsPressed()) {
+		char ch = UART3GetChar();
+//		UART3PutHex((int)ch);
+		if (cmdlen < sizeof(cmdstr)) {
+			cmdstr[cmdlen] = ch;
+			if ((ch == '\r') || (ch == '\n')) {
+				cmdstr[cmdlen] = '\0';			
+//				printf("\r\n");
+				if (strlen(cmdstr) > 0) {
+//					printf("\r\nfound command: %s\r\n", cmdstr);
+					UART3PutChar('\r');
+					command(cmdstr);
+					cmdlen = 0;
+				}
+			} else {
+				UART3PutChar(ch);
+				cmdlen++;
+			}
+		} else {
+			cmdlen = 0;
+		}
+	}
+}
+
---
+++ libCommon/read.c
@@ -0,0 +1,99 @@
+//#include <p33Exxxx.h>
+#include "p30sim.h"
+//#include <stdio.h>
+//#include "simio.h"
+
+extern int __C30_UART;
+
+extern volatile UxMODEBITS U3MODEbits __attribute__((__sfr__,weak));
+extern volatile UxSTABITS U3STAbits __attribute__((__sfr__,weak));
+extern volatile unsigned int U3RXREG __attribute__((__sfr__,weak));
+extern volatile unsigned int U3BRG __attribute__((__sfr__,weak));
+
+extern volatile UxMODEBITS U4MODEbits __attribute__((__sfr__,weak));
+extern volatile UxSTABITS U4STAbits __attribute__((__sfr__,weak));
+extern volatile unsigned int U4RXREG __attribute__((__sfr__,weak));
+extern volatile unsigned int U4BRG __attribute__((__sfr__,weak));
+
+int __attribute__((__weak__, __section__(".libc")))
+read(int handle, void *buffer, unsigned int len)
+{
+  int i;
+  volatile UxMODEBITS *umode = &U1MODEbits;
+  volatile UxSTABITS *ustatus = &U1STAbits;
+  volatile unsigned int *rxreg = &U1RXREG;
+  volatile unsigned int *brg = &U1BRG;
+
+  switch (handle)
+  {
+    case 0:
+    case 1:
+    case 2:
+#ifdef __C30_LEGACY_LIBC__
+      if (_Files[0]->_Lockno == 0)
+#endif
+      {
+//      if ((__C30_UART != 1) && (&U3BRG)) {
+//        umode = &U3MODEbits;
+//        ustatus = &U3STAbits;
+//        rxreg = &U3RXREG;
+//        brg = &U3BRG;
+//      }
+      if (__C30_UART == 2) {
+        umode = &U2MODEbits;
+        ustatus = &U2STAbits;
+        rxreg = &U2RXREG;
+        brg = &U2BRG;
+      }
+      if ((__C30_UART == 3) && (&U3BRG)) {
+        umode = &U3MODEbits;
+        ustatus = &U3STAbits;
+        rxreg = &U3RXREG;
+        brg = &U3BRG;
+      }
+      if ((__C30_UART == 4) && (&U4BRG)) {
+        umode = &U4MODEbits;
+        ustatus = &U4STAbits;
+        rxreg = &U4RXREG;
+        brg = &U4BRG;
+      }
+        if ((umode->UARTEN) == 0)
+        {
+          *brg = 0;
+          umode->UARTEN = 1;
+        }
+        for (i = len; i; --i)
+        {
+          int nTimeout;
+
+          /*
+          ** Timeout is 16 cycles per 10-bit char
+          */
+          nTimeout = 16*10;
+          while (((ustatus->URXDA) == 0) && nTimeout) --nTimeout;
+          if ((ustatus->URXDA) == 0) break;
+          *(char*)buffer++ = *rxreg;
+        }
+        len -= i;
+        break;
+      }
+
+    default: {
+/*
+      SIMIO simio;
+      register PSIMIO psimio asm("w0");
+
+      psimio  = &simio;
+      simio.method = SIM_READ;
+      simio.u.read.handle = handle;
+      simio.u.read.buffer = buffer;
+      simio.u.read.len = len;
+      doread(psimio);
+
+      len = simio.u.read.len;
+ */
+      break;
+    }
+  }
+  return(len);
+}
