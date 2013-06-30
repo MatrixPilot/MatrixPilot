c2115a9c7a3ebfa6a05fb296d71abceb12f4effa
Pete Hollands <peter.hollands@gmail.com>
2013-06-30 06:48:03 +0000

    branch MatrixPilot_4_x:  Take out a new additional 1000m*1000m T3 butterfly course that had appeared in waypoints.h and revert waypoints.h to it's normal default waypoints.
    git-svn-id: https://gentlenav.googlecode.com/svn/branches/MatrixPilot_4_x@2716 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

--- MatrixPilot/waypoints.h
+++ MatrixPilot/waypoints.h
@@ -137,11 +137,11 @@
 // 
 // By default the only waypoint is defined to be 75 meters above the starting point.
 
-/*
+
 const struct waypointDef waypoints[] = {
 		{ {   0,   0, 75 } , F_NORMAL, CAM_VIEW_LAUNCH } ,  // return to, and loiter 75 meters above the startup position
 };
- */
+ 
 
 /*
 const struct waypointDef waypoints[] = {
@@ -152,33 +152,6 @@ const struct waypointDef waypoints[] = {
 		{ {   50,   50  , 750 } , F_LOITER + F_TRIGGER + F_LAND, CAM_VIEW_LAUNCH } ,
 };
  */
-
- // CORNER is the absolute value of the X or Y coordinate at the corners of the course. 
-#define CORNER 1000
-
-// CLEARANCE is an allowance for obstacles.
-#define CLEARANCE 250
-
-#define CAM_VIEW_2  { CORNER, CORNER, 0 } // Define a Camera ViewPoint to look at 100 ,100, 0
-
-// Here is the T3 course definition:
-
-const struct waypointDef waypoints[] = {
-		{ {    CORNER  ,    CORNER  , CLEARANCE + 100 } , F_NORMAL, CAM_VIEW_LAUNCH } ,
-		{ {    CORNER  ,  - CORNER  , CLEARANCE +  75 } , F_NORMAL, CAM_VIEW_LAUNCH } ,
-		{ {  - CORNER  ,    CORNER  , CLEARANCE +  50 } , F_NORMAL, CAM_VIEW_LAUNCH } ,
-		{ {  - CORNER  ,  - CORNER  , CLEARANCE +  25 } , F_NORMAL, CAM_VIEW_LAUNCH } ,
-		{ {    CORNER  ,    CORNER  , CLEARANCE +  50 } , F_NORMAL, CAM_VIEW_LAUNCH } ,
-		{ {    CORNER  ,  - CORNER  , CLEARANCE +  75 } , F_NORMAL, CAM_VIEW_LAUNCH } ,
-		{ {  - CORNER  ,    CORNER  , CLEARANCE + 100 } , F_NORMAL, CAM_VIEW_LAUNCH } ,
-		{ {  - CORNER  ,  - CORNER  , CLEARANCE +  75 } , F_NORMAL, CAM_VIEW_2 } ,
-		{ {    CORNER  ,    CORNER  , CLEARANCE +  50 } , F_NORMAL, CAM_VIEW_2 } ,
-		{ {    CORNER  ,  - CORNER  , CLEARANCE +  25 } , F_NORMAL, CAM_VIEW_2 } ,
-		{ {  - CORNER  ,    CORNER  , CLEARANCE +  50 } , F_NORMAL, CAM_VIEW_2 } ,
-		{ {  - CORNER  ,  - CORNER  , CLEARANCE +  75 } , F_NORMAL, CAM_VIEW_2 } ,
-};
-
-
 ////////////////////////////////////////////////////////////////////////////////
 // rtlWaypoints[]
 // 
