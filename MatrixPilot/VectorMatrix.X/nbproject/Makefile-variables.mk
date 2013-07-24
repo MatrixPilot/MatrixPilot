e39cb859c99088b7e41d840a120e074516773c92
Mark Whitehorn <kd0aij@gmail.com>
2013-07-24 18:45:40 +0000

    branch MatrixPilot_mw4: update VectorMatrix.X project for UDB4/5 and AUAV3
    git-svn-id: https://gentlenav.googlecode.com/svn/branches/MatrixPilot_mw4@2839 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

---
+++ MatrixPilot/VectorMatrix.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,20 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# UDB4_5 configuration
+CND_ARTIFACT_DIR_UDB4_5=dist/UDB4_5/production
+CND_ARTIFACT_NAME_UDB4_5=VectorMatrix.X.a
+CND_ARTIFACT_PATH_UDB4_5=dist/UDB4_5/production/VectorMatrix.X.a
+CND_PACKAGE_DIR_UDB4_5=${CND_DISTDIR}/UDB4_5/package
+CND_PACKAGE_NAME_UDB4_5=VectorMatrix.X.tar
+CND_PACKAGE_PATH_UDB4_5=${CND_DISTDIR}/UDB4_5/package/VectorMatrix.X.tar
+# AUAV3 configuration
+CND_ARTIFACT_DIR_AUAV3=dist/AUAV3/production
+CND_ARTIFACT_NAME_AUAV3=VectorMatrix.X.a
+CND_ARTIFACT_PATH_AUAV3=dist/AUAV3/production/VectorMatrix.X.a
+CND_PACKAGE_DIR_AUAV3=${CND_DISTDIR}/AUAV3/package
+CND_PACKAGE_NAME_AUAV3=VectorMatrix.X.tar
+CND_PACKAGE_PATH_AUAV3=${CND_DISTDIR}/AUAV3/package/VectorMatrix.X.tar
