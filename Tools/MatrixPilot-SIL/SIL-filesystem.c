916de3ca24d8c9df6b96b2738021ad9b6fc0a460
Robert Dickenson <robert.dickenson@gmail.com>
2014-01-06 01:54:15 +0000

    MatrixPilot_trunk: addition of microsoft visual C++ 2010 express project for building MatrixPilot-SIL
    git-svn-id: https://gentlenav.googlecode.com/svn/trunk@3143 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

---
+++ Tools/MatrixPilot-SIL/SIL-filesystem.c
@@ -0,0 +1,40 @@
+//
+//  SIL-serial.c
+//  MatrixPilot-SIL
+//
+//  Created by Ben Levitt on 2/10/13.
+//  Copyright (c) 2013 MatrixPilot. All rights reserved.
+//
+
+#if (WIN == 1 || NIX == 1)
+
+#include "defines.h"
+#include "../libUDB/heartbeat.h"
+#include "SIL-filesystem.h"
+//#include "libUDB.h"
+//#include <stdio.h>
+
+
+int FSInit(void)
+{
+	return 1;
+}
+
+FSFILE* FSfopen(const char* fileName, const char* mode)
+{
+//	printf("opening logfile %s\r\n", fileName);
+	return fopen(fileName, mode);
+}
+
+int FSfclose(FSFILE* fo)
+{
+	return fclose(fo);
+}
+
+size_t FSfwrite(const void* data_to_write, size_t size, size_t n, FSFILE* stream)
+{
+	return fwrite(data_to_write, size, n, stream);
+}
+
+
+#endif // (WIN == 1 || NIX == 1)
