d4f4ff2e1d007b3fd17bdebd633a1619146cac60
Mark Whitehorn <kd0aij@gmail.com>
2014-07-26 03:19:44 +0000

    branch _mw4fbw: project Cub-auav3: set gains for flight test of rudder fbw and nav
    git-svn-id: https://gentlenav.googlecode.com/svn/branches/MatrixPilot_mw4fbw@3270 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

--- MatrixPilot/Cub-auav3/projectSpecific/options.h
+++ MatrixPilot/Cub-auav3/projectSpecific/options.h
@@ -518,9 +518,9 @@
 // MANUAL_AILERON_RUDDER_MIX is the fraction of manual aileron control to mix into the rudder when
 // in stabilized or waypoint mode.  This mainly helps aileron-initiated turning while in stabilized.
 // RUDDER_BOOST is the additional gain multiplier for the manually commanded rudder deflection
-#define YAWKP_RUDDER				0.5 // 0.1
+#define YAWKP_RUDDER				0.2 // 0.1
 #define YAWKD_RUDDER				0 //0.03 // 0.1
-#define ROLLKP_RUDDER				0.04
+#define ROLLKP_RUDDER				0.1
 #define ROLLKD_RUDDER				0 //0.05
 #define MANUAL_AILERON_RUDDER_MIX	0.0
 #define RUDDER_BOOST				0.5
