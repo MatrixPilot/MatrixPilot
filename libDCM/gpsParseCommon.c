bd8cf37bc9b53971578956aeb592dcde5dadeecb
Robert Dickenson <robert.dickenson@gmail.com>
2014-08-06 22:27:46 +0000

    MatrixPilot_mjp_helicalTurns: missed one module with the 'rotate' function name change
    git-svn-id: https://gentlenav.googlecode.com/svn/branches/MatrixPilot_wjp_helicalTurns@3292 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

--- libDCM/gpsParseCommon.c
+++ libDCM/gpsParseCommon.c
@@ -270,7 +270,7 @@ static void udb_background_callback_triggered(void)
 		accum_velocity.WW = (__builtin_mulss(sine(actual_dir), ground_velocity_magnitudeXY) << 2) + 0x00008000;
 		GPSvelocity.y = accum_velocity._.W1;
 
-		rotate(&location_deltaXY, cog_delta); // this is a key step to account for rotation effects!!
+		rotate_2D(&location_deltaXY, cog_delta); // this is a key step to account for rotation effects!!
 
 		GPSlocation.x = location[0] + location_deltaXY.x;
 		GPSlocation.y = location[1] + location_deltaXY.y;
