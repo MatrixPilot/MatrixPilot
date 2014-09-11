3a35242e05759a7a76ece4b75288aedb565a1d60
Robert Dickenson <robert.dickenson@gmail.com>
2014-09-11 12:35:25 +0000

    MatrixPilot_trunk: very minor changes to bring trunk into line with my development branch. DPRINT some feedback in LedTest program, cleanup of I2C1 driver, and introduce some stardardising of types in altitude module. (last commit somehow missed I2C1.c)
    git-svn-id: https://gentlenav.googlecode.com/svn/trunk@3325 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

--- libUDB/I2C1.c
+++ libUDB/I2C1.c
@@ -54,7 +54,6 @@ static void I2C1_doneWrite(void);
 static void I2C1_writeCommandData(void);
 static void serviceI2C1(void);              // service the I2C
 
-static int16_t I2C1ERROR = 0;
 static boolean I2C1_Busy = true;            // Port busy flag. Set true until initialized
 static uint16_t I2C1_Index = 0;             // index into the write buffer
 static uint8_t I2C1_AddressByte = 0;
@@ -95,7 +94,7 @@ static void serviceI2C1(void) // service the I2C
 	{
 		I2C1_state = &I2C1_idle;        // disable response to any interrupts
 		I2C1_Init();                    // turn the I2C back on
-		// Put something here to reset state machine.  Make sure attached servies exit nicely.
+		// Put something here to reset state machine.  Make sure attached services exit nicely.
 	}
 }
 
