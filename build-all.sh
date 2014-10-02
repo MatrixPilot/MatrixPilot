f9adac2af4be3fb88cfd0b35c41bc3dbbe620fe1
Robert Dickenson <robert.dickenson@gmail.com>
2014-10-02 15:01:26 +0000

    MatrixPilot_trunk: fix relative include path and cleanup unix shell script
    git-svn-id: https://gentlenav.googlecode.com/svn/trunk@3380 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

--- build-all.sh
+++ build-all.sh
@@ -5,31 +5,22 @@ cd build
 
 mkdir SILSIM
 cd SILSIM
-pwd
 make -f ../../makefile SOURCE_DIR=../.. DEVICE=SILSIM
 cd ..
-pwd
-read -p "Press [Enter] key to continue"
 
 mkdir UDB5
 cd UDB5
-pwd
 make -f ../../makefile SOURCE_DIR=../.. DEVICE=UDB5 TOOLCHAIN=XC16
 cd ..
-read -p "Press [Enter] key to continue"
 
 mkdir AUAV3
 cd AUAV3
-pwd
 make -f ../../makefile SOURCE_DIR=../.. DEVICE=AUAV3 TOOLCHAIN=XC16
 cd ..
-read -p "Press [Enter] key to continue"
 
 mkdir UDB4
 cd UDB4
-pwd
 make -f ../../makefile SOURCE_DIR=../.. DEVICE=UDB4 TOOLCHAIN=XC16
 cd ..
-read -p "Press [Enter] key to continue"
 
 cd ..
