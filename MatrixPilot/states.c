1c03091ddb1c5c7444377b4de9a6120d9592f19e
William Premerlani <wpremerlani@gmail.com>
2014-10-08 22:55:38 +0000

    branch MatrixPilot_wjp_helicalTurns: raised STANDBY_PAUSE from 24 seconds to 48 seconds   to assure decay of GPS startup transient prior to recording of origin during a fast warm start.   For example, it was found that although the EM506 is very sensitive and accurate, during a warm start   it can produce a 3D nav solution too soon, before the initialization transient had time to dissipate.
    git-svn-id: https://gentlenav.googlecode.com/svn/branches/MatrixPilot_wjp_helicalTurns@3396 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

--- MatrixPilot/states.c
+++ MatrixPilot/states.c
@@ -37,11 +37,11 @@ uint8_t counter = 0;
 #define STANDBY_PAUSE (24 * FSM_CLK)    // pause for 24 seconds of runs through the state machine
 #else
 #define CALIB_PAUSE (10.5 * FSM_CLK)    // wait for 10.5 seconds of runs through the state machine
-#define STANDBY_PAUSE (24 * FSM_CLK)    // pause for 24 seconds of runs through the state machine
+#define STANDBY_PAUSE (48 * FSM_CLK)    // pause for 48 seconds of runs through the state machine
+										// Standby pause was raised from 24 seconds to 48 seconds by BillP
+										// to improve the accuracy of the origin during a fast warm start
 #endif
 
-//#define CALIB_PAUSE (10.5 * FSM_CLK)    // wait for 10.5 seconds of runs through the state machine
-//#define STANDBY_PAUSE (24 * FSM_CLK)    // pause for 24 seconds of runs through the state machine
 #define NUM_WAGGLES 4                   // waggle 4 times during the end of the standby pause (this number must be less than STANDBY_PAUSE)
 #define WAGGLE_SIZE 300
 
