8899d3c43bbfa69e82ee338ad503a240cf3242a0
Robert Dickenson <robert.dickenson@gmail.com>
2015-01-11 21:19:57 +0000

    MatrixPilot_trunk: update the *nix build script
    git-svn-id: https://gentlenav.googlecode.com/svn/trunk@3458 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

--- Tools/Build/make-build.sh
+++ Tools/Build/make-build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-python pyProjectCreator.py -t UDB4  -o ../../build -c ../Config -d Config -d MatrixPilot -d libUDB -d libDCM -d libFlashFS -d libVectorMatrix -d MAVLink -d Microchip -d Tools/MatrixPilot-SIL
-python pyProjectCreator.py -t UDB5  -o ../../build -c ../Config -d Config -d MatrixPilot -d libUDB -d libDCM -d libFlashFS -d libVectorMatrix -d MAVLink -d Microchip -d Tools/MatrixPilot-SIL
-python pyProjectCreator.py -t AUAV3 -o ../../build -c ../Config -d Config -d MatrixPilot -d libUDB -d libDCM -d libFlashFS -d libVectorMatrix -d MAVLink -d Microchip -d Tools/MatrixPilot-SIL
-python pyProjectCreator.py -t SIL   -o ../../build -c ../Config -d Config -d MatrixPilot -d libDCM -d MAVLink -d Tools/MatrixPilot-SIL
+python pyProjectCreator.py -t SIL  -r ../..  -o ../../build -d Config -d MatrixPilot -d libDCM -d MAVLink -d Tools/MatrixPilot-SIL
 
-python pyProjectCreator.py -n RollPitchYaw -t UDB4  -o ../../build -c ../../RollPitchYaw -d RollPitchYaw -d libUDB -d libDCM -d libVectorMatrix
-python pyProjectCreator.py -n RollPitchYaw -t UDB5  -o ../../build -c ../../RollPitchYaw -d RollPitchYaw -d libUDB -d libDCM -d libVectorMatrix
-python pyProjectCreator.py -n RollPitchYaw -t AUAV3 -o ../../build -c ../../RollPitchYaw -d RollPitchYaw -d libUDB -d libDCM -d libVectorMatrix
+python pyProjectCreator.py -t UDB4 -r ../.. -o ../../build -d Config -d MatrixPilot -d libUDB -d libDCM -d libFlashFS -d libVectorMatrix -d MAVLink -d Microchip -d Tools/MatrixPilot-SIL
+python pyProjectCreator.py -t UDB5 -r ../..  -o ../../build -d Config -d MatrixPilot -d libUDB -d libDCM -d libFlashFS -d libVectorMatrix -d MAVLink -d Microchip -d Tools/MatrixPilot-SIL
+python pyProjectCreator.py -t AUAV3 -r ../.. -o ../../build -d Config -d MatrixPilot -d libUDB -d libDCM -d libFlashFS -d libVectorMatrix -d MAVLink -d Microchip -d Tools/MatrixPilot-SIL
 
-python pyProjectCreator.py -n LedTest -t UDB4  -o ../../build -c ../../Tools/LedTest -d Tools/LedTest -d libUDB
-python pyProjectCreator.py -n LedTest -t UDB5  -o ../../build -c ../../Tools/LedTest -d Tools/LedTest -d libUDB
-python pyProjectCreator.py -n LedTest -t AUAV3 -o ../../build -c ../../Tools/LedTest -d Tools/LedTest -d libUDB
+#python pyProjectCreator.py -n RollPitchYaw -t UDB4  -o ../../build -c ../../RollPitchYaw -d RollPitchYaw -d libUDB -d libDCM -d libVectorMatrix
+#python pyProjectCreator.py -n RollPitchYaw -t UDB5  -o ../../build -c ../../RollPitchYaw -d RollPitchYaw -d libUDB -d libDCM -d libVectorMatrix
+#python pyProjectCreator.py -n RollPitchYaw -t AUAV3 -o ../../build -c ../../RollPitchYaw -d RollPitchYaw -d libUDB -d libDCM -d libVectorMatrix
+
+#python pyProjectCreator.py -n LedTest -t UDB4  -o ../../build -c ../../Tools/LedTest -d Tools/LedTest -d libUDB
+#python pyProjectCreator.py -n LedTest -t UDB5  -o ../../build -c ../../Tools/LedTest -d Tools/LedTest -d libUDB
+#python pyProjectCreator.py -n LedTest -t AUAV3 -o ../../build -c ../../Tools/LedTest -d Tools/LedTest -d libUDB
