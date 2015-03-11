077e43e07435cd88ff934e7ec966d4e987402eed
Robert Dickenson <robert.dickenson@gmail.com>
2015-03-11 14:59:01 +0000

    MatrixPilot_wjp_helicalTurns: revert parameter name change and add explanatory comment - to be consistent with trunk (no functional change)
    git-svn-id: https://gentlenav.googlecode.com/svn/branches/MatrixPilot_wjp_helicalTurns@3513 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

--- libDCM/mathlibNAV.c
+++ libDCM/mathlibNAV.c
@@ -73,7 +73,7 @@ int16_t sine(int8_t angle)
 	}
 }
 
-int8_t arcsine(int16_t y)
+int8_t arcsine(int16_t y)  // arcsine takes the y coordinate of an x,y point and returns an angle
 {
 	// returns the inverse sine of y
 	// y is in Q2.14 format, 16384 is maximum value
