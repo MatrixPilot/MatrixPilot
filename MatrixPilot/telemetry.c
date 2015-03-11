de181cb920084978a2488c62f4044322ab25cfa4
Pete Hollands <peter.hollands@gmail.com>
2015-03-11 14:52:59 +0000

    Branch Helical Turns: Fix mistake in SERIAL_UDB_EXTR in F14 format statement serial_output statment.
    git-svn-id: https://gentlenav.googlecode.com/svn/branches/MatrixPilot_wjp_helicalTurns@3512 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

--- MatrixPilot/telemetry.c
+++ MatrixPilot/telemetry.c
@@ -577,7 +577,7 @@ void serial_output_8hz(void)
                         break;
 		case 6:
 			serial_output("F14:WIND_EST=%i:GPS_TYPE=%i:DR=%i:BOARD_TYPE=%i:AIRFRAME=%i:"
-			              "RCON=0x%X:TRAP_FLAGS=0x%X:TRAP_SOURCE=0x%lX:ALARMS=%i:\r\n",
+			              "RCON=0x%X:TRAP_FLAGS=0x%X:TRAP_SOURCE=0x%lX:ALARMS=%i:"
 			              "CLOCK=%i:FP=%d:\r\n",
 			    WIND_ESTIMATION, GPS_TYPE, DEADRECKONING, BOARD_TYPE, AIRFRAME_TYPE,
 			    get_reset_flags(), trap_flags, trap_source, osc_fail_count,
