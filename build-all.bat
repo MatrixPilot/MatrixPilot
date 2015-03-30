ebadf9c709c67464db22f9b4862d84ab5c1c1fea
Robert Dickenson <robert.dickenson@gmail.com>
2015-03-30 12:56:06 +0000

    MatrixPilot_trunk: updated makefile invocation script and added em::blocks template for the project generator
    git-svn-id: https://gentlenav.googlecode.com/svn/trunk@3553 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

--- build-all.bat
+++ build-all.bat
@@ -1,36 +1,42 @@
 @echo off
+
 mkdir build >nul 2>&1
 pushd build
 
 mkdir SILSIM >nul 2>&1
 pushd SILSIM
-make -f ../../makefile SOURCE_DIR=../.. DEVICE=SILSIM
+make -j 8 -f ../../makefile DEVICE=SIL
 popd
 
-mkdir AUAV3 >nul 2>&1
-pushd AUAV3
-make -f ../../makefile SOURCE_DIR=../.. DEVICE=AUAV3 TOOLCHAIN=C30
+mkdir PX4 >nul 2>&1
+pushd PX4
+make -j 8 -f ../../makefile DEVICE=PX4
 popd
-mkdir UDB4 >nul 2>&1
-pushd UDB4
-make -f ../../makefile SOURCE_DIR=../.. DEVICE=UDB4 TOOLCHAIN=C30
+
+mkdir AUAV3-C30 >nul 2>&1
+pushd AUAV3-C30
+make -j 8 -f ../../makefile DEVICE=AUAV3 TOOLCHAIN=C30
 popd
-mkdir UDB5 >nul 2>&1
-pushd UDB5
-make -f ../../makefile SOURCE_DIR=../.. DEVICE=UDB5 TOOLCHAIN=C30
+mkdir UDB4-C30 >nul 2>&1
+pushd UDB4-C30
+make -j 8 -f ../../makefile DEVICE=UDB4 TOOLCHAIN=C30
+popd
+mkdir UDB5-C30 >nul 2>&1
+pushd UDB5-C30
+make -j 8 -f ../../makefile DEVICE=UDB5 TOOLCHAIN=C30
 popd
 
 mkdir AUAV3-XC16 >nul 2>&1
 pushd AUAV3-XC16
-make -f ../../makefile SOURCE_DIR=../.. DEVICE=AUAV3 TOOLCHAIN=XC16
+make -j 8 -f ../../makefile DEVICE=AUAV3 TOOLCHAIN=XC16
 popd
 mkdir UDB4-XC16 >nul 2>&1
 pushd UDB4-XC16
-make -f ../../makefile SOURCE_DIR=../.. DEVICE=UDB4 TOOLCHAIN=XC16
+make -j 8 -f ../../makefile DEVICE=UDB4 TOOLCHAIN=XC16
 popd
 mkdir UDB5-XC16 >nul 2>&1
 pushd UDB5-XC16
-make -f ../../makefile SOURCE_DIR=../.. DEVICE=UDB5 TOOLCHAIN=XC16
+make -j 8 -f ../../makefile DEVICE=UDB5 TOOLCHAIN=XC16
 popd
 
 popd
