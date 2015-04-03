a1b753f5b1ef83b609d3b524d0081739215fd386
Robert Dickenson <robert.dickenson@gmail.com>
2015-04-03 02:24:00 +0000

    MatrixPilot_trunk: clean up a long standing annoying warning on I2C1_DRIVER and add some initial placeholder modules to libSTM support working towards achieving minimal clean functional build for such targets
    git-svn-id: https://gentlenav.googlecode.com/svn/trunk@3564 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

---
+++ libSTM/libSTM.c
@@ -0,0 +1,414 @@
+//
+//  libSTM.c
+//  MatrixPilot
+//
+//  Created by Robert Dickenson on 17/6/2014.
+//  Copyright (c) 2014 MatrixPilot. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../libUDB/libUDB.h"
+#include "../libUDB/ADchannel.h"
+#include "../libUDB/heartbeat.h"
+#include "../libUDB/magnetometer.h"
+#include "magnetometerOptions.h"
+#include "../libUDB/events.h"
+#include "../libUDB/osd.h"
+#include "../libUDB/mpu6000.h"
+
+#include "../libUDB/uart.h"
+
+#if (BOARD_TYPE == PX4_BOARD)
+
+#include "stm32f4xx_hal.h"
+#include "usart.h"
+
+//#include "cmsis_os.h"
+//#include "ff.h"
+//#include "ff_gen_drv.h"
+//#include "sd_diskio.h" /* defines SD_Driver as external */
+
+//#include "SIL-config.h"
+//#include "SIL-udb.h"
+//#include "SIL-ui.h"
+//#include "SIL-events.h"
+//#include "SIL-eeprom.h"
+
+uint16_t udb_heartbeat_counter;
+
+int16_t udb_pwIn[MAX_INPUTS];   // pulse widths of radio inputs
+int16_t udb_pwTrim[MAX_INPUTS]; // initial pulse widths for trimming
+int16_t udb_pwOut[MAX_OUTPUTS]; // pulse widths for servo outputs
+
+union udb_fbts_byte udb_flags;
+
+struct ADchannel udb_xaccel, udb_yaccel, udb_zaccel;    // x, y, and z accelerometer channels
+struct ADchannel udb_xrate, udb_yrate, udb_zrate;       // x, y, and z gyro channels
+struct ADchannel udb_vref;                              // reference voltage
+struct ADchannel udb_analogInputs[4];
+
+int16_t udb_magFieldBody[3];                    // magnetic field in the body frame of reference
+int16_t udb_magOffset[3] = { 0 , 0 , 0 };       // magnetic offset in the body frame of reference
+int16_t magGain[3] = { RMAX , RMAX , RMAX };    // magnetometer calibration gains
+int16_t rawMagCalib[3] = { 0 , 0 , 0 };
+uint8_t magreg[6];                              // magnetometer read-write buffer
+int16_t magFieldRaw[3];
+
+union longww battery_current;                   // battery_current._.W1 is in tenths of Amps
+union longww battery_mAh_used;                  // battery_mAh_used._.W1 is in mAh
+union longww battery_voltage;                   // battery_voltage._.W1 is in tenths of Volts
+uint8_t rc_signal_strength;                     // rc_signal_strength is 0-100 as percent of full signal
+
+int16_t magMessage;
+int16_t vref_adj;
+
+volatile uint16_t trap_flags;
+volatile uint32_t trap_source;
+volatile uint16_t osc_fail_count;
+uint16_t mp_rcon = 3;                           // default RCON state at normal powerup
+
+extern int mp_argc;
+extern char **mp_argv;
+
+uint8_t leds[5] = {0, 0, 0, 0, 0};
+uint8_t sil_radio_on;
+
+// Functions only included with nv memory.
+#if (USE_NV_MEMORY == 1)
+UDB_SKIP_FLAGS udb_skip_flags = {0, 0, 0};
+
+void udb_skip_radio_trim(boolean b)
+{
+	udb_skip_flags.skip_radio_trim = 1;
+}
+
+void udb_skip_imu_calibration(boolean b)
+{
+	udb_skip_flags.skip_imu_cal = 1;
+}
+#endif
+
+uint8_t udb_cpu_load(void)
+{
+	return 5; // sounds reasonable for a fake cpu%
+}
+
+int16_t udb_servo_pulsesat(int32_t pw)
+{
+	if (pw > SERVOMAX) pw = SERVOMAX;
+	if (pw < SERVOMIN) pw = SERVOMIN;
+	return (int16_t)pw;
+}
+
+void udb_servo_record_trims(void)
+{
+	int16_t i;
+
+	for (i = 1; i <= NUM_INPUTS; i++)
+	{
+		udb_pwTrim[i] = udb_pwIn[i];
+//		DPRINT("udb_pwTrim[%i] = %u\r\n", i, udb_pwTrim[i]);
+	}
+}
+
+void udb_set_action_state(boolean newValue)
+{
+	// not simulated
+	(void)newValue;   // unused parameter
+}
+
+void udb_a2d_record_offsets(void)
+{
+	UDB_XACCEL.offset = UDB_XACCEL.value;
+	udb_xrate.offset  = udb_xrate.value;
+	UDB_YACCEL.offset = UDB_YACCEL.value - (Y_GRAVITY_SIGN ((int16_t)(2*GRAVITY))); // opposite direction
+	udb_yrate.offset  = udb_yrate.value;
+	UDB_ZACCEL.offset = UDB_ZACCEL.value;
+	udb_zrate.offset  = udb_zrate.value;
+	udb_vref.offset   = udb_vref.value;
+}
+
+uint16_t get_reset_flags(void)
+{
+	return mp_rcon;
+}
+
+#if (MAG_YAW_DRIFT == 1)
+
+static magnetometer_callback_funcptr magnetometer_callback = NULL;
+
+void rxMagnetometer(magnetometer_callback_funcptr callback)
+{
+	magnetometer_callback = callback;
+}
+
+void I2C_doneReadMagData(void)
+{
+	magFieldRaw[0] = (magreg[0]<<8) + magreg[1];
+	magFieldRaw[1] = (magreg[2]<<8) + magreg[3];
+	magFieldRaw[2] = (magreg[4]<<8) + magreg[5];
+
+	if (magMessage == 7)
+	{
+		udb_magFieldBody[0] = MAG_X_SIGN((__builtin_mulsu((magFieldRaw[MAG_X_AXIS]), magGain[MAG_X_AXIS]))>>14)-(udb_magOffset[0]>>1);
+		udb_magFieldBody[1] = MAG_Y_SIGN((__builtin_mulsu((magFieldRaw[MAG_Y_AXIS]), magGain[MAG_Y_AXIS]))>>14)-(udb_magOffset[1]>>1);
+		udb_magFieldBody[2] = MAG_Z_SIGN((__builtin_mulsu((magFieldRaw[MAG_Z_AXIS]), magGain[MAG_Z_AXIS]))>>14)-(udb_magOffset[2]>>1);
+
+		if ((abs(udb_magFieldBody[0]) < MAGNETICMAXIMUM) &&
+			(abs(udb_magFieldBody[1]) < MAGNETICMAXIMUM) &&
+			(abs(udb_magFieldBody[2]) < MAGNETICMAXIMUM))
+		{
+//			udb_magnetometer_callback();
+			if (magnetometer_callback != NULL)
+			{
+				magnetometer_callback();
+			}
+			else
+			{
+				printf("ERROR: magnetometer_callback function pointer not set\r\n");
+			}
+		}
+		else
+		{
+			magMessage = 0;         // invalid reading, reset the magnetometer
+		}
+	}
+}
+
+void HILSIM_MagData(magnetometer_callback_funcptr callback)
+{
+	(void)callback;
+//	magnetometer_callback = callback;
+	magMessage = 7;                 // indicate valid magnetometer data
+	I2C_doneReadMagData();          // run the magnetometer computations
+}
+
+#endif // MAG_YAW_DRIFT
+
+int16_t FindFirstBitFromLeft(int16_t val)
+{
+	int16_t i = 0;
+
+	if (val != 0)
+	{
+		for (i = 1; i <= 16; i++)
+		{
+			if (val & 0x8000) break;
+			val <<= 1;
+		}
+	}
+	return i;
+}
+
+void udb_led_toggle(uint8_t x)
+{
+    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+}
+void led_on(uint8_t x)
+{
+//void HAL_GPIO_WritePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState);
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 1);
+}
+void led_off(uint8_t x)
+{
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 0);
+}
+
+int32_t gps_baud_rate = 0;
+int32_t serial_baud_rate = 0;
+
+void udb_gps_start_sending_data(void) {}
+void udb_gps_set_rate(int32_t rate) { gps_baud_rate = rate; }
+boolean udb_gps_check_rate(int32_t rate) { return (gps_baud_rate == rate); }
+
+void udb_serial_start_sending_data(void) {}
+void udb_serial_set_rate(int32_t rate) { serial_baud_rate = rate; }
+
+uint16_t register_event_p(void (*event_callback)(void), eventPriority priority) { return 0; }
+void trigger_event(uint16_t hEvent) {}
+
+void osd_init(void) {}
+void osd_reset(void) {}
+void osd_spi_init(void) {}
+void osd_spi_write(int8_t address, int8_t byte) {}
+void osd_spi_write_byte(int8_t byte) {}
+void osd_spi_write_location(int16_t loc) {}
+void osd_spi_write_string(const uint8_t* str) {}
+void osd_spi_write_vertical_string_at_location(int16_t loc, const uint8_t* str) {}
+void osd_spi_erase_chars(uint8_t n) {}
+void osd_spi_write_number(int32_t val, int8_t num_digits, int8_t decimal_places, int8_t num_flags, int8_t header, int8_t footer) {}
+
+void filesys_init(void) {}
+
+//static jmp_buf default_jmp_buf;
+
+int setjmp(void)
+{
+	return 0;
+}
+
+void __delay32(unsigned long cycles)
+{
+    /* Insert delay in ms */
+//    HAL_Delay(cycles);
+}
+
+//#if (CONSOLE_UART == 0)
+//void console(void) {}
+//#endif // CONSOLE_UART
+
+//void Reset_Handler(void) {} // this must be loosely defined in the startup code and the default seems to call main()
+//void mcu_init(void) {} // now provided by main() in the STMCubeMX generated code (redef'd to mcu_init()
+
+void heartbeat(void) // called from MPU6000 ISR
+{
+}
+
+void udb_init(void)
+{
+	udb_heartbeat_counter = 0;
+	udb_flags.B = 0;
+	MPU6000_init16(&heartbeat);
+}
+
+void udb_run(void)
+{
+}
+
+void udb_background_trigger(background_callback callback)
+{
+	if (callback) callback();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+#ifdef __GNUC__
+  /* With GCC/RAISONANCE, small printf (option LD Linker->Libraries->Small printf
+     set to 'Yes') calls __io_putchar() */
+  #define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+  #define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif /* __GNUC__ */
+
+/**
+  * @brief  Retargets the C library printf function to the USART.
+  * @param  None
+  * @retval None
+  */
+PUTCHAR_PROTOTYPE
+{
+// CONSOLE_UART
+  /* Place your implementation of fputc here */
+  /* e.g. write a character to the EVAL_COM1 and Loop until the end of transmission */
+  HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, 0xFFFF);
+
+  return ch;
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+  // we want to signal the process responsible for this uart
+}
+
+//void HAL_UART_RxHalfCpltCallback(UART_HandleTypeDef *huart);
+void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart)
+{
+
+}
+/*
+void vApplicationIdleHook(void)
+{
+	{
+#if (USE_TELELOG == 1)
+		telemetry_log();
+#endif
+#if (USE_USB == 1)
+		USBPollingService();
+#endif
+#if (CONSOLE_UART != 0 && SILSIM == 0)
+		console();
+#endif
+		udb_run();
+	}
+}
+ */
+
+void ClrError(void)
+{
+//	if (U##x##STAbits.OERR) U##x##STAbits.OERR = 0;
+}
+
+uint8_t buffered_char = 0;
+uint8_t buffered_full = 0;
+
+char IsPressed(void)
+{
+	HAL_StatusTypeDef status;
+	uint8_t Data[2];
+
+// HAL_StatusTypeDef HAL_UART_Receive(UART_HandleTypeDef *huart, uint8_t *pData, uint16_t Size, uint32_t Timeout)
+
+	if (buffered_full == 1)
+	{
+		return 1;
+	}
+
+	status = HAL_UART_Receive(&huart2, &Data[0], 1, 0);
+	if (status == HAL_OK)
+	{
+		buffered_char = Data[0];
+		buffered_full = 1;
+		return 1;
+	}
+
+//	if (U##x##STAbits.URXDA)
+	return 0;
+}
+
+char GetChar(void)
+{
+	HAL_StatusTypeDef status;
+	uint8_t Data[2];
+	char Temp;
+//	while (!IsPressed());
+//	Temp = U##x##RXREG;
+
+	if (buffered_full == 1)
+	{
+		Temp = buffered_char;
+		buffered_full = 0;
+	}
+	else
+	{
+		status = HAL_UART_Receive(&huart6, &Data[0], 1, 0);
+		if (status == HAL_OK)
+		{
+			Temp = Data[0];
+		}
+	}
+//	ClrError();
+	return Temp;
+}
+
+void PutChar(char ch)
+{
+	HAL_StatusTypeDef status;
+
+//HAL_StatusTypeDef HAL_UART_Transmit(UART_HandleTypeDef *huart, uint8_t *pData, uint16_t Size, uint32_t Timeout)
+
+	status = HAL_UART_Transmit(&huart6, &ch, 1, 0);
+	if (status == HAL_OK)
+	{
+	}
+
+//	U##x##TXREG = ch;
+//	while (U##x##STAbits.TRMT == 0);
+}
+
+//void _init(void) {}
+
+#endif // (BOARD_TYPE == PX4_BOARD)
---
+++ libSTM/mpu_spi.c
@@ -0,0 +1,352 @@
+// This file is part of MatrixPilot.
+//
+//    http://code.google.com/p/gentlenav/
+//
+// Copyright 2009-2011 MatrixPilot Team
+// See the AUTHORS.TXT file for a list of authors of MatrixPilot.
+//
+// MatrixPilot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MatrixPilot is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with MatrixPilot.  If not, see <http://www.gnu.org/licenses/>.
+
+
+#include "../libUDB/libUDB.h"
+
+#if (BOARD_TYPE != UDB4_BOARD)
+
+#include "../libUDB/oscillator.h"
+#include "../libUDB/interrupt.h"
+#include "mpu_spi.h"
+//#include <delay.h>
+//#include <spi.h>
+
+#if (MPU_SPI == 1)
+
+#define MPU_SS       SPI1_SS
+#define MPU_SS_TRIS  SPI1_TRIS
+#define _SPIRBF      SPI1STATbits.SPIRBF
+#define _SPIROV      SPI1STATbits.SPIROV
+#define _SRXMPT      SPI1STATbits.SRXMPT
+#define _SPIBEC      SPI1STATbits.SPIBEC
+#define _SRMPT       SPI1STATbits.SRMPT
+#define _SPIIF       _SPI1IF
+#define _SPIIP       _SPI1IP
+#define _SPIIE       _SPI1IE
+#define SPIBUF       SPI1BUF
+#define SPISTAT      SPI1STAT
+#define OpenSPI      OpenSPI1
+#define CloseSPI     CloseSPI1
+#define ConfigIntSPI ConfigIntSPI1
+#define SPIInterrupt _SPI1Interrupt
+
+#elif (MPU_SPI == 2)
+
+#define MPU_SS       SPI2_SS
+#define MPU_SS_TRIS  SPI2_TRIS
+#undef  _SPIRBF
+#define _SPIRBF      SPI2STATbits.SPIRBF
+#undef  _SPIROV
+#define _SPIROV      SPI2STATbits.SPIROV
+#define _SRXMPT      SPI2STATbits.SRXMPT
+#define _SPIBEC      SPI2STATbits.SPIBEC
+#define _SRMPT       SPI2STATbits.SRMPT
+#define _SPIIF       _SPI2IF
+#define _SPIIP       _SPI2IP
+#define _SPIIE       _SPI2IE
+#define SPIBUF       SPI2BUF
+#define SPISTAT      SPI2STAT
+#define OpenSPI      OpenSPI2
+#define CloseSPI     CloseSPI2
+#define ConfigIntSPI ConfigIntSPI2
+#define SPIInterrupt _SPI2Interrupt
+
+#else
+#error "Select either 1 or 2 for MPU SPI."
+#endif
+
+
+static void no_call_back(void);
+
+static void (*mpu_call_back)(void) = &no_call_back;
+/*
+static uint16_t* SPI_data;
+static uint8_t SPI_high;
+static uint8_t SPI_low;
+#ifndef __dsPIC33E__
+static int16_t SPI_i;
+static int16_t SPI_j;
+static int16_t SPI_n;
+#endif
+ */
+
+// Configure SPI module in 16-bit master mode
+void initMPUSPI_master16(uint16_t priPre, uint16_t secPre)
+{
+/*
+	uint16_t SPICON1Value;      // holds the information about SPI configuration
+	uint16_t SPICON2Value;
+	uint16_t SPISTATValue;      // holds the information about SPI Enable/Disable
+
+	MPU_SS = 1;                 // deassert MPU SS
+	MPU_SS_TRIS = 0;            // make MPU SS an output
+	CloseSPI();                 // turn off SPI module
+	ConfigIntSPI(SPI_INT_DIS & SPI_INT_PRI_6);
+
+#if defined(__dsPIC33E__)
+	SPICON1Value =
+	    ENABLE_SDO_PIN & SPI_MODE16_ON & ENABLE_SCK_PIN &
+	    SPI_SMP_OFF & SPI_CKE_OFF &
+	    SLAVE_ENABLE_OFF &
+	    CLK_POL_ACTIVE_LOW &
+	    MASTER_ENABLE_ON &
+	    secPre & priPre;
+	SPICON2Value = FRAME_ENABLE_OFF & FRAME_SYNC_OUTPUT; // & FIFO_BUFFER_DISABLE;
+	SPISTATValue = SPI_ENABLE & SPI_IDLE_CON & SPI_RX_OVFLOW_CLR & BUF_INT_SEL_5;
+	// BUF_INT_SEL_5 == Interrupt when the last bit is shifted out of SPIxSR, and the transmit is complete
+#else
+	SPICON1Value =
+	    ENABLE_SDO_PIN & SPI_MODE16_ON & ENABLE_SCK_PIN &
+	    SPI_SMP_ON & SPI_CKE_OFF &
+	    SLAVE_ENABLE_OFF &
+	    CLK_POL_ACTIVE_LOW &
+	    MASTER_ENABLE_ON &
+	    secPre & priPre;
+	SPICON2Value = FRAME_ENABLE_OFF & FRAME_SYNC_OUTPUT;
+	SPISTATValue = SPI_ENABLE & SPI_IDLE_CON & SPI_RX_OVFLOW_CLR;
+#endif
+
+#ifdef __PIC32MX__
+//	 * Example: OpenSPI1(SPI_MODE32_ON|SPI_SMP_ON|MASTER_ENABLE_ON|SEC_PRESCAL_1_1|PRI_PRESCAL_1_1, SPI_ENABLE);
+	OpenSPI(SPICON1Value, SPISTATValue);
+#else
+	OpenSPI(SPICON1Value, SPICON2Value, SPISTATValue);
+//	printf("SPI1STAT %04X, SPI1CON1 %04X, SPI1CON2 %04X\r\n", SPI1STAT, SPI1CON1, SPI1CON2);
+#endif
+
+	_SPIROV = 0;                // clear SPI receive overflow
+	_SPIIF  = 0;                // clear any pending interrupts
+	_SPIIP  = INT_PRI_MPUSPI;   // set interrupt priority
+//	_SPIIE  = 1;                // turn on SPI interrupts
+ */
+ }
+
+// Blocking 16 bit write to SPI
+void writeMPUSPIreg16(uint16_t addr, uint16_t data)
+{
+/*
+	int16_t k;
+
+	MPU_SS = 0;                 // assert chip select
+	k = SPIBUF;
+	_SPIIE = 0;                 // ensure the interrupt is disabled
+	_SPIIF = 0;                 // ensure the interrupt flag is clear
+	SPIBUF = addr << 8 | data;  // send address and data
+#if 1
+	while (!_SPIIF);            // wait for transfer to complete
+	_SPIIF = 0;                 // clear interrupt flag
+#else
+	delay_us(32+2);             // allow 16 cycles at 500kHz for the write
+#endif
+	k = SPIBUF;                 // dump received data
+	MPU_SS = 1;                 // deassert chip select
+	// this delay is necessary; it appears that SS must be deasserted for one or
+	// more SPI clock cycles between writes
+//	delayUs(1);
+	delay_us(1);
+ */
+ }
+
+static void no_call_back(void)
+{
+}
+
+#if defined(__dsPIC33E__)
+
+// SPI module has 8 word FIFOs
+// burst read 2n bytes starting at addr;
+// Since first byte is address, max of 15 data bytes may be transferred with n=7
+void readMPUSPI_burst16n(uint16_t data[], int16_t n, uint16_t addr, void (*call_back)(void))
+{
+	uint16_t i;
+
+	MPU_SS = 0;                 // assert chip select
+	mpu_call_back = call_back;  // store the address of the call back routine
+	SPI_data = &data[0];        // store address of data buffer
+	i = SPIBUF;                 // empty read buffer
+	addr |= 0x80;               // write address-1 in high byte + n-1 dummy words to TX FIFO
+	SPIBUF = addr << 8;         // issue read command
+	for (i = 0; i < n; i++) {
+		SPIBUF = 0;             // queue 'n' null words into the SPI transmit buffer
+	}
+	_SPIIE = 1;                 // turn on SPI interrupts
+}
+
+// this ISR empties the RX FIFO into the SPI_data buffer
+// no possibility of overrun if buffer length is at least 8 words
+void __attribute__((__interrupt__, __no_auto_psv__)) SPIInterrupt(void)
+{
+	uint16_t spibuf;
+
+	_SPIIF = 0;                 // clear interrupt flag as soon as possible so as to not miss any interrupts
+	indicate_loading_inter;
+	interrupt_save_set_corcon;
+	_SPIIE = 0;                 // turn off SPI interrupts
+	spibuf = SPIBUF;            // get first byte from first word
+	SPI_high = 0xFF & spibuf;
+
+	while (!_SRXMPT) {          // empty the FIFO
+		spibuf = SPIBUF;
+		SPI_low = spibuf >> 8;
+		*SPI_data++ = SPI_high << 8 | SPI_low;
+		SPI_high = 0xFF & spibuf;
+	}
+	MPU_SS = 1;
+	(*mpu_call_back)();
+	interrupt_restore_corcon;
+}
+
+#else // no SPI FIFO
+
+// burst read 2n bytes starting at addr
+void readMPUSPI_burst16n(uint16_t data[], int16_t n, uint16_t addr, void (*call_back)(void))
+{
+/*
+	uint16_t spibuf;
+
+	MPU_SS = 0;                 // assert chip select
+	mpu_call_back = call_back;  // save address of call back routine
+	SPI_i = 0;                  // initialize indices
+	SPI_j = 0;
+	SPI_n = n;
+	SPI_data = &data[0];        // save address of data buffer
+	addr |= 0x80;
+	spibuf = SPIBUF;
+	SPIBUF = addr << 8;         // issue read command
+	_SPIIE = 1;                 // turn on SPI interrupts
+ */
+ }
+
+void __attribute__((__interrupt__, __no_auto_psv__)) SPIInterrupt(void)
+{
+/*
+	uint16_t spibuf;
+
+	_SPIIF = 0;                 // clear interrupt flag as soon as possible so as to not miss any interrupts
+	indicate_loading_inter;
+	interrupt_save_set_corcon;
+#if 1
+	if (SPI_i == 0) {
+		spibuf = SPIBUF;                               // could move this to before the conditional
+		SPIBUF = 0x0000;
+		SPI_high = 0xFF & spibuf;                      // could move this to after the conditional
+		SPI_i = 1;
+	} else if (SPI_i < SPI_n) {
+		spibuf = SPIBUF;                               // could move this to before the conditional
+		SPIBUF = 0x0000;
+		SPI_low = spibuf >> 8;                         // could move this to before the conditional
+		*(SPI_data + SPI_j) = SPI_high << 8 | SPI_low; // could move this to before the conditional
+		SPI_high = 0xFF & spibuf;                      // could move this to after the conditional
+		SPI_i++;
+		SPI_j++;
+	} else {
+		spibuf = SPIBUF;                               // could move this to before the conditional
+		SPI_low = spibuf >> 8;                         // could move this to before the conditional
+		*(SPI_data + SPI_j) = SPI_high << 8 | SPI_low; // could move this to before the conditional
+		MPU_SS = 1;
+		_SPIIE = 0;             // turn off SPI interrupts
+		(*mpu_call_back)();
+	}
+#else
+	spibuf = SPIBUF;
+	SPI_low = spibuf >> 8;
+	*(SPI_data + SPI_j) = SPI_high << 8 | SPI_low;
+	SPI_i++;
+	if (SPI_i == 1) {
+		SPIBUF = 0x0000;
+	} else if (SPI_i <= SPI_n) {
+		SPIBUF = 0x0000;
+		SPI_j++;
+	} else {
+		MPU_SS = 1;
+		_SPIIE = 0;             // turn off SPI interrupts
+		(*mpu_call_back)();
+	}
+	SPI_high = 0xFF & spibuf;
+#endif // 0
+	interrupt_restore_corcon;
+ */
+ }
+
+#endif // (__dsPIC33E__)
+
+uint16_t readMPUSPIreg16(uint16_t addr)
+{
+	int16_t data = 0;
+/*
+#if defined(__dsPIC33E__)
+	while (_SRXMPT == 0)        // clear receive FIFO
+#endif
+	{
+		data = SPIBUF;          // empty receive buffer
+	}
+	MPU_SS = 0;                 // assert chip select
+	_SPIIE = 0;                 // ensure the interrupt is disabled
+	_SPIIF = 0;                 // ensure the interrupt flag is clear
+	addr |= 0x80;               // set the read bit in addr byte
+	SPIBUF = addr << 8;         // issue read command
+//	while (!_SRMPT);            // wait for transfer to complete
+	while (!_SPIIF);            // wait for transfer to complete
+	_SPIIF = 0;                 // clear interrupt flag
+	data = SPIBUF;
+	MPU_SS = 1;
+//	delayUs(40);
+	delay_us(40);
+ */
+ 	return data;
+}
+
+#if 0 // experimental blocking 8 bit read for dsPIC33EP
+
+// FIXME: why doesn't this work? read FIFO is all zeros even though non-zero data is observed on MISO
+uint8_t readMPUSPIreg16(uint16_t addr)
+{
+	int16_t k, data[8];
+
+//	while (_SRXMPT == 0) {
+//		data[k] = SPIBUF;       // clear receive FIFO
+//	}
+	k = SPISTAT;
+	_SPIROV = 0;
+	MPU_SS = 0;                 // assert chip select
+	addr |= 0x80;               // set the read bit in addr byte
+	SPIBUF = addr << 8;         // issue read command
+	while (_SPIBEC);            // wait for TX FIFO to empty
+	while (!_SRMPT);            // wait for last transfer to complete
+	MPU_SS = 1;
+	delay_us(20);
+	MPU_SS = 0;
+	SPIBUF = addr << 8;         // issue read command
+	while (_SPIBEC);            // wait for TX FIFO to empty
+	while (!_SRMPT);            // wait for last transfer to complete
+	data[0] = SPIBUF;
+	data[1] = SPIBUF;
+//	for (k = 0; k < 8; k++) {
+//		data[k] = SPIBUF;       // read one word from FIFO
+//	}
+	MPU_SS = 1;                 // deassert chip select for a while
+	delay_us(40);
+	return 0xFF & data[0];
+}
+
+#endif // 0
+
+#endif // BOARD_TYPE
