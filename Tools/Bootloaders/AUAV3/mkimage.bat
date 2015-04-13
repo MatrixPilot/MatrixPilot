944c1a85fc5fae14d7394cae9ba0d3ed25a15ccf
Robert Dickenson <robert.dickenson@gmail.com>
2015-04-13 13:44:01 +0000

    MatrixPilot_nucleo: remove directories irrelevant to branch in order to reduce diff strength
    git-svn-id: https://gentlenav.googlecode.com/svn/branches/MatrixPilot_nucleo@3585 cf4e4d4a-e611-11dd-8fb1-9960f2a117f8

--- Tools/Bootloaders/AUAV3/mkimage.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-@rem python hex2src.py --pad=FF --range=FF8000:FFFFFF "AUAV3 Bootloader.hex" > bootimage.s
-python hex2src.py --pad=FF --range=FF8000:FFFFFF "AUAV3 Bootloader.hex" "bootimage.s"
