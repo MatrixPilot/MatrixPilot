619ec29aec197059bd2aac2f201e89220dbe8575
Robert Dickenson <robert@beaglebone.(none)>
2015-12-26 13:06:10 +0000

    modified after testing build on BBB

--- build-all.sh
+++ build-all.sh
@@ -5,22 +5,7 @@ cd build
 
 mkdir -p SILSIM
 cd SILSIM
-make -f ../../makefile SOURCE_DIR=../.. DEVICE=SILSIM
-cd ..
-
-mkdir -p UDB5
-cd UDB5
-make -f ../../makefile SOURCE_DIR=../.. DEVICE=UDB5 TOOLCHAIN=XC16
-cd ..
-
-mkdir -p AUAV3
-cd AUAV3
-make -f ../../makefile SOURCE_DIR=../.. DEVICE=AUAV3 TOOLCHAIN=XC16
-cd ..
-
-mkdir -p UDB4
-cd UDB4
-make -f ../../makefile SOURCE_DIR=../.. DEVICE=UDB4 TOOLCHAIN=XC16
+make -f ../../makefile DEVICE=SILSIM
 cd ..
 
 cd ..
