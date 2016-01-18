d4d76006c46efc3e7bcc1441eb0a7c6be9139961
Peter Hollands <peter.hollands@gmail.com>
2016-01-18 22:16:45 +0000

    Bug fix: correct printf style arguments to serial_output() for variable aero_force. Thanks to Kees (Glideruav) for reporting this.

--- MatrixPilot/telemetry.c
+++ MatrixPilot/telemetry.c
@@ -715,7 +715,7 @@ void telemetry_output_8hz(void)
 				serial_output("imx%i:imy%i:imz%i:lex%i:ley%i:lez%i:fgs%X:ofc%i:tx%i:ty%i:tz%i:G%d,%d,%d:AF%i,%i,%i:",IMUlocationx._.W1,IMUlocationy._.W1,IMUlocationz._.W1,
 				    locationErrorEarth[0], locationErrorEarth[1], locationErrorEarth[2],
 				    state_flags.WW, osc_fail_count,
-				    IMUvelocityx._.W1, IMUvelocityy._.W1, IMUvelocityz._.W1, goal.x, goal.y, goal.z);
+                    IMUvelocityx._.W1, IMUvelocityy._.W1, IMUvelocityz._.W1, goal.x, goal.y, goal.z, aero_force[0], aero_force[1], aero_force[2]);
 //				serial_output("tmp%i:prs%li:alt%li:agl%li:",
 //				    get_barometer_temperature(), get_barometer_pressure(), 
 //				    get_barometer_alt(), get_barometer_agl());
