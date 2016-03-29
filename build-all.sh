29ef82b94db991df1e2b89fc45cbd6563f17789c
Robert Dickenson <robert.dickenson@gmail.com>
2016-03-29 17:10:18 +1100

    Updated to make the travis build succeed

--- build-all.sh
+++ build-all.sh
@@ -3,24 +3,26 @@
 mkdir -p build
 cd build
 
-mkdir -p SILSIM
-cd SILSIM
-make -f ../../makefile SOURCE_DIR=../.. DEVICE=SILSIM
-cd ..
-
-mkdir -p UDB5
-cd UDB5
-make -f ../../makefile SOURCE_DIR=../.. DEVICE=UDB5 TOOLCHAIN=XC16
+mkdir -p SIL
+cd SIL
+make -j 8 -f ../../makefile DEVICE=SIL
 cd ..
 
 mkdir -p AUAV3
 cd AUAV3
-make -f ../../makefile SOURCE_DIR=../.. DEVICE=AUAV3 TOOLCHAIN=XC16
+make -j 8 -f ../../makefile DEVICE=AUAV3 TOOLCHAIN=XC16
+cd ..
+
+mkdir -p UDB5
+cd UDB5
+make -j 8 -f ../../makefile DEVICE=UDB5 TOOLCHAIN=XC16
 cd ..
 
 mkdir -p UDB4
 cd UDB4
-make -f ../../makefile SOURCE_DIR=../.. DEVICE=UDB4 TOOLCHAIN=XC16
+make -j 8 -f ../../makefile DEVICE=UDB4 TOOLCHAIN=XC16
 cd ..
+
+find | grep *.hex
 
 cd ..
