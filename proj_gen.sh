c7500234f688261773d78cb464794f9ed2848f53
Robert Dickenson <robert.dickenson@gmail.com>
2016-03-29 16:21:39 +1100

    add the project generator scripts

---
+++ proj_gen.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#python ./Tools/Build/pyProjectCreator.py -n MatrixPilot -t SIL -c Config/Cessna
+#python ./Tools/Build/pyProjectCreator.py -n MatrixPilot -t SIL -c Config/CloudsFly
+python ./Tools/Build/pyProjectCreator.py -n MatrixPilot -t PX4 -c Config/Cessna -d HILSIM
+
+exit 0
+
+python ./Tools/Build/pyProjectCreator.py -n MatrixPilot -t UDB4
+python ./Tools/Build/pyProjectCreator.py -n MatrixPilot -t UDB5
+python ./Tools/Build/pyProjectCreator.py -n MatrixPilot -t AUAV3
+
+python ./Tools/Build/pyProjectCreator.py -n RollPitchYaw -t UDB4
+python ./Tools/Build/pyProjectCreator.py -n RollPitchYaw -t UDB5
+python ./Tools/Build/pyProjectCreator.py -n RollPitchYaw -t AUAV3
+
+python ./Tools/Build/pyProjectCreator.py -n LedTest -t UDB4
+python ./Tools/Build/pyProjectCreator.py -n LedTest -t UDB5
+python ./Tools/Build/pyProjectCreator.py -n LedTest -t AUAV3
