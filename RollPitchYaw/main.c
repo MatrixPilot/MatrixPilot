fc7f5a237bda45e12c84860e4c9915b6a238cb3d
Peter Hollands <peter.hollands@gmail.com>
2016-08-09 07:08:21 +0100

    RollPitchYaw: Initialise the Usart using callback pointers.

--- RollPitchYaw/main.c
+++ RollPitchYaw/main.c
@@ -46,8 +46,15 @@ int main(void)
 	// Set up the libraries
 	udb_init();
 	dcm_init();
+#ifndef SERIAL_BAUDRATE
+#define SERIAL_BAUDRATE 19200 // default
+#warning SERIAL_BAUDRATE set to default value of 19200 bps
+#endif
+    udb_serial_set_rate(SERIAL_BAUDRATE);
 
-	udb_serial_set_rate(SERIAL_BAUDRATE);
+#if (CONSOLE_UART != 2)
+	udb_init_USART(&udb_serial_callback_get_byte_to_send, &udb_serial_callback_received_byte);
+#endif
 
 	led_off(LED_GREEN);
 
