503f340ebd613ccccabc2e9a85a7bef8b3e28f18
Pete Hollands <peter.hollands@gmail.com>
2016-09-15 22:20:40 +0100

    Fix linker/loader flag in X16 toolchain makefile (#52)
    Improve output of build-all.sh  and show Hex files generated by the script.

--- build-all.sh
+++ build-all.sh
@@ -3,26 +3,32 @@
 mkdir -p build
 cd build
 
+echo
 mkdir -p SIL
 cd SIL
 make -j 8 -f ../../makefile DEVICE=SIL
 cd ..
 
+echo
 mkdir -p AUAV3
 cd AUAV3
 make -j 8 -f ../../makefile DEVICE=AUAV3 TOOLCHAIN=XC16
 cd ..
 
+echo
 mkdir -p UDB5
 cd UDB5
 make -j 8 -f ../../makefile DEVICE=UDB5 TOOLCHAIN=XC16
 cd ..
 
+echo
 mkdir -p UDB4
 cd UDB4
 make -j 8 -f ../../makefile DEVICE=UDB4 TOOLCHAIN=XC16
 cd ..
 
-find | grep *.hex
-
 cd ..
+echo
+echo "The following hex files now exist:-"
+find build | grep hex
+echo
