f69927cc0d3cc8b4f756842af7f9c62092d954f2
Peter Hollands <peter.hollands@gmail.com>
2016-10-14 12:16:08 +0100

    FlashOSD: Bring osd_options.h up to date with those in the main MatrixPilot project. Remove legacy options.h, and os_config.h files that are no longer used.

--- Tools/FlashOSD/Config/options_osd.h
+++ Tools/FlashOSD/Config/options_osd.h
@@ -20,19 +20,24 @@
 
 
 // define the OSD types
-#define OSD_NONE            1   // OSD disabled
+#define OSD_NONE            0   // OSD disabled
 #define OSD_NATIVE          1   // native OSD
 #define OSD_REMZIBI         2   // Output data formatted to use as input to a Remzibi OSD
 #define OSD_MINIM           3   // Output data formatted for minim OSD
 
 
 // define USE_OSD to one of the types above to enables that system
-#define USE_OSD         OSD_NONE
-//#define USE_OSD         OSD_NATIVE
+//#define USE_OSD         OSD_NONE
+#define USE_OSD         OSD_NATIVE
 //#define USE_OSD         OSD_REMZIBI
 //#define USE_OSD         OSD_MINIM
 
 
 // OSD_NATIVE development options:
-#define USE_OSD_SPI     0   // set this to 1 to use the SPI peripheral, 0 to bit-bash
-#define OSD_SF          5   // scale factor for SPI delays - TODO: get rid of
+// Set OSD_LEGACY_SPI_PINOUT to 1 if you wired up your UDB4 or UDB5 for the Native OSD
+// before October 2016; In this case, The wiring does not follow the labels on the UDB boards.
+// Otherwise, leave OSD_LEGACY_SPI_PINOUT_UDB as 0, for the pin out labels on the UDB boards
+// to agree with the SPI signals of the OSD.
+#define OSD_LEGACY_SPI_PINOUT   0   // Default setting is 0
+#define USE_OSD_SPI             0   // set this to 1 to use the SPI peripheral, 0 to bit-bash
+#define OSD_SF                  5   // scale factor for SPI delays - TODO: get rid of
