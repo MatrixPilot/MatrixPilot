fe664dcafbad336aa6a1b3d3e3d07d7fc2b62dca
Peter Hollands <peter.hollands@gmail.com>
2016-10-14 12:54:00 +0100

    RollPitchYaw and LedTest: Bring options_osd.h up to date

--- Tools/LedTest/Config/options_osd.h
+++ Tools/LedTest/Config/options_osd.h
@@ -27,12 +27,17 @@
 
 
 // define USE_OSD to one of the types above to enables that system
-#define USE_OSD         OSD_NONE
+#define USE_OSD           OSD_NONE
 //#define USE_OSD         OSD_NATIVE
 //#define USE_OSD         OSD_REMZIBI
 //#define USE_OSD         OSD_MINIM
 
 
 // OSD_NATIVE development options:
-#define USE_OSD_SPI     0   // set this to 1 to use the SPI peripheral, 0 to bit-bash
-#define OSD_SF          5   // scale factor for SPI delays - TODO: get rid of
+// Set OSD_LEGACY_SPI_PINOUT to 1 if you wired up your UDB4 or UDB5 for the Native OSD
+// before October 2016; In this case, The wiring does not follow the labels on the UDB boards.
+// Otherwise, leave OSD_LEGACY_SPI_PINOUT_UDB as 0, for the pin out labels on the UDB boards
+// to agree with the SPI signals of the OSD.
+#define OSD_LEGACY_SPI_PINOUT   0   // Default setting is 0
+#define USE_OSD_SPI             0   // set this to 1 to use the SPI peripheral, 0 to bit-bash
+#define OSD_SF                  5   // scale factor for SPI delays - TODO: get rid of
