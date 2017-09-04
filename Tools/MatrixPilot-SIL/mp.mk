f3d1747facb7fd049f53ab6784f94246a3eee360
Robert Dickenson <robert.dickenson@gmail.com>
2017-09-04 19:17:51 +1000

    re-enable the build script with SILSIM and XPlane plugin targets as well as fix and cleanup the SILSIM local makefiles

--- Tools/MatrixPilot-SIL/mp.mk
+++ Tools/MatrixPilot-SIL/mp.mk
@@ -9,14 +9,17 @@ MP_OBJECTS = \
 ../../MatrixPilot/data_services.o \
 ../../MatrixPilot/data_storage.o \
 ../../MatrixPilot/euler_angles.o \
+../../MatrixPilot/flightplan.o \
 ../../MatrixPilot/flightplan-logo.o \
 ../../MatrixPilot/flightplan-waypoints.o \
+../../MatrixPilot/helicalTurnCntrl.o \
 ../../MatrixPilot/main.o \
 ../../MatrixPilot/MAVFlexiFunctions.o \
 ../../MatrixPilot/MAVLink.o \
 ../../MatrixPilot/MAVMission.o \
 ../../MatrixPilot/MAVParams.o \
 ../../MatrixPilot/MAVUDBExtra.o \
+../../MatrixPilot/minIni.o \
 ../../MatrixPilot/mode_switch.o \
 ../../MatrixPilot/mp_osd.o \
 ../../MatrixPilot/navigate.o \
