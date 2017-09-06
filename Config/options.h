151ab01a1c904b02e0c08cb8f9b1f7a54b93b658
Peter Hollands <peter.hollands@gmail.com>
2017-09-06 11:41:10 +0100

    GNSS: metrics not used by HILSIM or SILSIM

--- Config/options.h
+++ Config/options.h
@@ -78,6 +78,7 @@
 // You can specify a level of good GNSS reception before MatrixPilot accepts "GPS ACQUIRED".
 // You can generally leaves these lines at their default values. A value of zero switches off the check.
 // The VDOP parameter is only available for Ublox GNSS devices. It is ignored for other GNSS units.
+// The metrics are not used by HILSIM or SILSIM.
 
 #define GNSS_HDOP_REQUIRED_FOR_STARTUP       20  //  Horizontal Dilution of Precision
 #define GNSS_VDOP_REQUIRED_FOR_STARTUP	     60  //  Vertical Dilution of Precision
