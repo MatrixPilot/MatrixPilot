5076fc78e5049b000646df9db522d60e3c54765f
Peter Hollands <peter.hollands@gmail.com>
2018-02-18 20:34:34 +0000

    Update telemetry.c: Add Helical Control Variables; Increase serial buffer size.

--- MatrixPilot/helicalTurnCntrl.c
+++ MatrixPilot/helicalTurnCntrl.c
@@ -87,6 +87,7 @@
 int16_t tiltError[3];
 int16_t desiredRotationRateRadians[3];
 int16_t rotationRateError[3];
+int16_t desiredTurnRateRadians;
 //int16_t angleOfAttack;
 
 static int16_t estimatedLift;
@@ -203,7 +204,6 @@ void helicalTurnCntrl(void)
 	int16_t rtlkick;
 	int16_t desiredPitch;
 	int16_t steeringInput;
-	int16_t desiredTurnRateRadians;
 	int16_t desiredTiltVector[3];
 	int16_t desiredRotationRateGyro[3];
 	uint16_t airSpeed;
