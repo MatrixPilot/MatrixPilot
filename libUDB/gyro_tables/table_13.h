4f4a59e9d4367d5127b74ef1d2e068c941d2ce96
William Premerlani <wjpremerlani@gmail.com>
2023-01-16 05:20:51 -0500

    luge_imu branch : updated residual offsets for SN7 and SN13

--- libUDB/gyro_tables/table_13.h
+++ libUDB/gyro_tables/table_13.h
@@ -8,7 +8,7 @@
 #define LOOKUP_LSB_MASK 0x00FF
 #define MSB_SHIFT 8
 
-int16_t residual_offset[] = { 0 , 0 , 0 } ;
+int16_t residual_offset[] = { 2795 , -71 , -1231 } ;
 
 #define TABLE_ORIGIN -18309
 
