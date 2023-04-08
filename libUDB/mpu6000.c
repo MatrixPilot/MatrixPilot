4a3bc2096cd9287e51c5355d50e686302398ced1
Ben Levitt <benjie@gmail.com>
2023-04-08 14:16:52 -0700

    Update mpu6000.c for communications with the new ICM_20600

--- libUDB/mpu6000.c
+++ libUDB/mpu6000.c
@@ -99,12 +99,13 @@ void MPU6000_init16(callback_fptr_t fptr)
 	
 	// Which chip is this?
 	uint16_t mpu_whoami = readMPUSPIreg16(MPUREG_WHOAMI) ;
-	is_ICM_20689 = (mpu_whoami == WHOAMI_ICM_20689) ;
-//	mpu_whoami = readMPUSPIreg16(MPUREG_WHOAMI) ;
+	is_ICM_20689 = (mpu_whoami == WHOAMI_ICM_20689);
 
 	// Disable I2C bus (recommended on datasheet)
-	writeMPUSPIreg16(MPUREG_USER_CTRL, BIT_I2C_IF_DIS);
-	
+    if (mpu_whoami != WHOAMI_ICM_20600) {
+    	writeMPUSPIreg16(MPUREG_USER_CTRL, BIT_I2C_IF_DIS);
+	}
+    
 	if (is_ICM_20689) {
 //	if (mpu_whoami == WHOAMI_ICM_20689 || mpu_whoami == WHOAMI_ICM_20600) {
 		// Disable I2C communications on the ICM_20689
@@ -138,8 +139,7 @@ void MPU6000_init16(callback_fptr_t fptr)
 #endif // GYRO_RANGE 
 
 	
-	if (is_ICM_20689) {
-//	if (mpu_whoami == WHOAMI_ICM_20689 || mpu_whoami == WHOAMI_ICM_20600) {
+	if (mpu_whoami == WHOAMI_ICM_20689 || mpu_whoami == WHOAMI_ICM_20600) {
 	#if (ACCEL_RANGE == 2)
 		writeMPUSPIreg16(MPUREG_ACCEL_CONFIG, 0<<3); // Accel scele 2g, g = 8192
 	#elif (ACCEL_RANGE == 4)
